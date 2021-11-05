babdbc00c9d3f6cef695e948f22390828dd77b50
krebsni <st151522@stud.uni-stuttgart.de>
2021-11-05 17:46:33 +0100

    save result and error functions added for intermediate step task results in multi-step plugins

--- qhana_plugin_runner/tasks.py
+++ qhana_plugin_runner/tasks.py
@@ -73,3 +73,63 @@ def save_task_error(self, failing_task_id: str, db_id: int):
     task_data.save(commit=True)
 
     result.forget()
+
+
+@CELERY.task(name=f"{_name}.save-step-result", bind=True, ignore_result=True)
+def save_step_result(self, task_log: str, db_id: int):
+    """Save the intermediate step task log in the database and update the final task status of the database task."""
+    if not isinstance(task_log, str):
+        raise TypeError(
+            f"The task log / task metadata must be of type str to be stored in the database! (expected str but got {type(task_log)})"
+        )
+
+    TASK_LOGGER.debug(f"Saving result for intermediate step task with db id '{db_id}'")
+    task_data: ProcessingTask = ProcessingTask.get_by_id(id_=db_id)
+    if task_data is None:
+        # TODO use better fitting error
+        raise KeyError(f"Could not find db entry for id {db_id}, saving task log failed!")
+
+    task_data.step_finished_status = "SUCCESS"
+    task_data.step_finished_at = datetime.utcnow()
+    if task_log is None:
+        # finished tasks must have a result string
+        task_data.step_log += ""
+    elif isinstance(task_log, str):
+        task_data.task_log += "\n" + task_log
+    else:
+        task_data.task_log += "\n" + repr(task_log)
+
+    task_data.save(commit=True)
+    TASK_LOGGER.debug(
+        f"Save task log for intermediate step task with db id '{db_id}' successful."
+    )
+
+    AsyncResult(self.request.parent_id, app=CELERY).forget()
+
+
+@CELERY.task(name=f"{_name}.save-step-error", bind=True, ignore_result=True)
+def save_task_error(self, failing_task_id: str, db_id: int):
+    """Save the error as the result of the root task in the database."""
+    result = AsyncResult(failing_task_id, app=CELERY)
+    exc = result.result
+    traceback = result.traceback
+
+    TASK_LOGGER.error(
+        f"Sub-Task {failing_task_id} of Task with db id {db_id} raised exception: {exc!r}\n{traceback}"
+    )
+
+    task_data: ProcessingTask = ProcessingTask.get_by_id(id_=db_id)
+
+    if task_data is None:
+        TASK_LOGGER.error(
+            f"Cannot save error for task with db id {db_id}, no db entry found!"
+        )
+        return  # TODO start new error logging task or save to extra db table
+
+    task_data.step_finished_status = result.state
+    task_data.step_finished_at = datetime.utcnow()
+    task_data.step_log += "\n" f"{exc!r}\n\n{traceback}"
+
+    task_data.save(commit=True)
+
+    result.forget()
