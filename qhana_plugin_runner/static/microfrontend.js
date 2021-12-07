9cfa812a4f4ea1115673025d3342471c270bfdd4
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2021-12-07 21:01:58 +0100

    Refactor microfrontends to make them usable inside iframes
    Output complete html documents instead of html fragments
    Include javascript communicate with parent iframe via message passing.
    This is a progressive enhancement meaning that the microfrontend
    is still functional on its own even without javascript.

---
+++ qhana_plugin_runner/static/microfrontend.js
@@ -0,0 +1,247 @@
+// Copyright 2021 QHAna plugin runner contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+/**
+ * Register the main message event listener on the current window.
+ */
+function registerMessageListener() {
+    // state that will be available for listeners in their closure
+    var state = {
+        href: window.location.href,
+        lastHeight: 0,
+        heightUnchangedCount: 0,
+    }
+
+    // main event listener, delegates events to dedicated listeners
+    window.addEventListener("message", (event) => {
+        var data = event.data;
+        if (typeof data === "string") {
+            // string message
+        } else {
+            if (data != null && data.type === "load-css") {
+                onLoadCssMessage(data, state);
+            }
+        }
+    });
+}
+
+/**
+ * Handle css load messages that request the micro frontend to load additional css files.
+ *
+ * @param {{type: 'load-css', urls: string[]}} data 
+ * @param {{lastHeight: number, heightUnchangedCount: number}} state 
+ */
+function onLoadCssMessage(data, state) {
+    var head = document.querySelector("head");
+    data.urls.forEach((url) => {
+        var styleLink = document.createElement("link");
+        styleLink.href = url;
+        styleLink.rel = "stylesheet";
+        head.appendChild(styleLink);
+    });
+    state.heightUnchangedCount = 0;
+    monitorHeightChanges(state);
+}
+
+/**
+ * Send a message to the parent window.
+ *
+ * @param {string|object} message the data attribute of the created message event
+ */
+function sendMessage(message) {
+    var targetWindow = window.opener || window.parent;
+    if (targetWindow) {
+        targetWindow.postMessage(message, "*");
+    } else {
+        console.warn("Failed to message parent window. Is this page loaded outside of an iframe?");
+    }
+}
+
+/**
+ * Monitor height changes for a certain time and inform the parent window if the height has changed.
+ *
+ * @param {{lastHeight: number, heightUnchangedCount: number}} state 
+ */
+function monitorHeightChanges(state) {
+    var newHeight = notifyParentWindowOnHeightChange(state.height);
+    if (state.height === newHeight) {
+        if (state.heightUnchangedCount > 60) { // allow for 60*50ms for height to settle
+            return;
+        }
+        state.heightUnchangedCount = (state.heightUnchangedCount || 0) + 1;
+    } else {
+        state.heightUnchangedCount = 0;
+        state.height = newHeight;
+    }
+    window.setTimeout(() => monitorHeightChanges(state), 50);
+}
+
+/**
+ * Measure the current height and inform the parent window if the height has changed compared to `lastHeight`.
+ *
+ * @param {number} lastHeight the last measured height returned by this method (default: 0)
+ * @returns the current measured height
+ */
+function notifyParentWindowOnHeightChange(lastHeight = 0) {
+    var height = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight);
+    if (height !== lastHeight) {
+        sendMessage({ type: "ui-resize", height: height });
+    }
+    return height;
+}
+
+/**
+ * Notify the parent window that a micro frontend was successfully loaded and is available to receive messages.
+ * 
+ * Must be called **after** the message listener was attached to the window!
+ */
+function notifyParentWindowOnLoad() {
+    sendMessage("ui-loaded");
+    notifyParentWindowOnHeightChange();
+}
+
+/**
+ * Instrument embedded html forms to listen for submit events.
+ */
+function instrumentForm() {
+    const forms = document.querySelectorAll('form.qhana-form');
+    forms.forEach(form => {
+        const privateInputs = new Set(); // inputs that should be censored (e.g. password inputs)
+        const dataInputs = new Set(); // inputs that contain urls pointing to data input
+        form.querySelectorAll('input[data-private],input[type=password]').forEach(inputElement => {
+            const name = inputElement.getAttribute('name');
+            if (name == null) {
+                console.warn('Input has no specified name but is marked as private!', inputElement);
+            } else {
+                privateInputs.add(name);
+            }
+        });
+        form.querySelectorAll('input[data-input]').forEach(inputElement => {
+            const name = inputElement.getAttribute('name');
+            if (name == null) {
+                console.warn('Input has no specified name but is marked as data input!', inputElement);
+            } else {
+                dataInputs.add(name);
+            }
+            // TODO instrument data inputs
+        });
+        form.addEventListener("submit", (event) => {
+            onFormSubmit(event, dataInputs, privateInputs)
+        });
+    });
+}
+
+function onFormSubmit(event, dataInputs, privateInputs) {
+    const form = event.target;
+    const submitter = event.submitter;
+    let submitTarget = submitter.getAttribute("data-target") || form.getAttribute("data-target");
+    if (submitTarget === "microfrontend") {
+        return; // will just be replaced with another micro frontend, nothing to do
+    }
+    if (submitTarget === "api") {
+        event.preventDefault(); // cancel submit; need to do this manually...
+        const formData = new FormData(form);
+        let formMethod = form.method;
+        let formAction = new URL(form.action);
+        if (submitter != null) {
+            formAction = new URL(submitter.formAction);
+            formMethod = submitter.formMethod || formMethod;
+        }
+        formMethod = formMethod || "post";
+        const submitUrl = formAction.toString();
+        submitFormData(formData, formAction, formMethod)
+            .then(
+                (response) => {
+                    if (response.status === 200) {
+
+                        const inputDataUrls = new Set();
+                        const processedFormData = new FormData();
+                        formData.forEach((entry, key) => {
+                            if (privateInputs.has(key)) {
+                                // censor private values
+                                processedFormData.append(key, '***');
+                                return;
+                            }
+                            // add all other values unchanged
+                            processedFormData.append(key, entry);
+                            // add data inputs to extra list
+                            if (dataInputs.has(key) && (typeof entry === 'string')) {
+                                inputDataUrls.add(entry);
+                            }
+                        });
+                        sendMessage({
+                            type: "form-submit",
+                            formData: (new URLSearchParams(processedFormData)).toString(),
+                            formDataType: "application/x-www-form-urlencoded",
+                            dataInputs: new Array(...inputDataUrls),
+                            submitUrl: submitUrl,
+                            resultUrl: response.url,
+                        });
+                    } else {
+                        response.json().then((jsonBody) => {
+                            sendMessage({
+                                type: "form-error",
+                                status: response.status,
+                                error: jsonBody,
+                            }, (error) => {
+                                sendMessage({
+                                    type: "form-error",
+                                    status: response.status,
+                                    error: "unknown error",
+                                });
+
+                            });
+                        });
+                    }
+                },
+                (error) => {
+                    // TODO notify in case of error
+                    sendMessage({
+                        type: "form-error",
+                        status: 500,
+                        error: "unknown error",
+                    });
+                }
+            );
+    }
+}
+
+function submitFormData(formData, formAction, formMethod) {
+
+    let data = new URLSearchParams(formData);
+    if (formMethod.toLowerCase() === "get") {
+        data.forEach((value, key) => {
+            formAction.searchParams.append(key, value);
+        })
+        return fetch(formAction.toString())
+    }
+
+    return fetch(formAction.toString(), {
+        body: data.toString(),
+        method: formMethod,
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    });
+}
+
+
+// Main script entry point /////////////////////////////////////////////////////
+
+
+// only execute functions if loaded from a parent window (e.g. inside an iframe)
+if (window.top !== window.self) {
+    instrumentForm();
+    registerMessageListener();
+    notifyParentWindowOnLoad();
+}
