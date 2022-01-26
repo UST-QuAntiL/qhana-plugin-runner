f088c318667ee071859fd3acafc67cd1ccd8637f
Philipp Wundrack <philipp.wundrack@live.de>
2022-01-26 11:43:42 +0100

    add input encoding to first QNN

--- plugins/hybrid_ae_pkg/backend/quantum/pytorch/pyquil/QNN1.py
+++ plugins/hybrid_ae_pkg/backend/quantum/pytorch/pyquil/QNN1.py
@@ -2,7 +2,7 @@ from typing import Tuple
 
 import numpy as np
 from pyquil import Program, get_qc
-from pyquil.gates import RY, RZ, CNOT, MEASURE
+from pyquil.gates import RY, RZ, CNOT, MEASURE, RX
 from pyquil.quilatom import MemoryReference
 
 
@@ -18,9 +18,13 @@ def create_circuit(q_num: int) -> Tuple[Program, int]:
     params_per_gate = 15
 
     p = Program()
+    input_values = p.declare("input", "REAL", q_num)
     params = p.declare("params", "REAL", gates_num * params_per_gate)
     ro = p.declare("ro", "BIT", q_num)
     param_offset = 0
+
+    for i in range(q_num):
+        p += RX(input_values[i], i)
 
     for i in range(q_num - 1):
         for j in range(q_num - 1 - i):
