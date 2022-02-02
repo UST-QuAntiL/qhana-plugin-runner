207466821af263315463670f45f4f091bdadc606
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2022-02-02 15:05:38 +0100

    Fix parameter in redirect url

--- plugins/hello_worl_multi_step/routes.py
+++ plugins/hello_worl_multi_step/routes.py
@@ -241,5 +241,5 @@ class Step1View(MethodView):
         db_task.save(commit=True)
 
         return redirect(
-            url_for("tasks-api.TaskView", db_id=str(db_id)), HTTPStatus.SEE_OTHER
+            url_for("tasks-api.TaskView", task_id=str(db_id)), HTTPStatus.SEE_OTHER
         )
