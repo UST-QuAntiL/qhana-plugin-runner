fdb65e93dff636549c6949e1e7d60b42c7bbdcba
krebsni <st151522@stud.uni-stuttgart.de>
2022-02-09 10:59:23 +0100

    removed redundant commit

--- plugins/hello_worl_multi_step/routes.py
+++ plugins/hello_worl_multi_step/routes.py
@@ -141,8 +141,6 @@ class ProcessView(MethodView):
         task.link_error(save_task_error.s(db_id=db_task.id))
         task.apply_async()
 
-        db_task.save(commit=True)
-
         return redirect(
             url_for("tasks-api.TaskView", task_id=str(db_task.id)), HTTPStatus.SEE_OTHER
         )
