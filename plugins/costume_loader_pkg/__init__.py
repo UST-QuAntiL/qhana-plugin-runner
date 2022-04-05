045e948efbad2581f32015af33deee03633f2715
Philipp Wundrack <philipp.wundrack@live.de>
2022-04-05 10:34:17 +0200

    replace star imports

--- plugins/costume_loader_pkg/__init__.py
+++ plugins/costume_loader_pkg/__init__.py
@@ -48,7 +48,7 @@ class CostumeLoader(QHAnaPluginBase):
 try:
     # It is important to import the routes **after** COSTUME_LOADER_BLP and CostumeLoader are defined, because they are
     # accessed as soon as the routes are imported.
-    from .routes import *
+    from . import routes
 except ImportError:
     # When running `poetry run flask install`, importing the routes will fail, because the dependencies are not
     # installed yet.
