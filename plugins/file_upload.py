1a0557c7cdd90f67289ca28513bca108c456b4ee
Philipp Wundrack <philipp.wundrack@live.de>
2022-04-11 16:29:38 +0200

    implement file upload plugin

--- plugins/file_upload.py
+++ plugins/file_upload.py
@@ -13,8 +13,7 @@
 # limitations under the License.
 
 from http import HTTPStatus
-from json import dumps, loads
-from tempfile import SpooledTemporaryFile
+from json import dumps
 from typing import Mapping, Optional
 
 import marshmallow as ma
@@ -28,6 +27,7 @@ from flask.templating import render_template
 from flask.views import MethodView
 from flask.wrappers import Response
 from marshmallow import EXCLUDE
+from werkzeug.utils import secure_filename
 
 from qhana_plugin_runner.api.plugin_schemas import (
     DataMetadata,
@@ -47,69 +47,67 @@ from qhana_plugin_runner.storage import STORE
 from qhana_plugin_runner.tasks import save_task_error, save_task_result
 from qhana_plugin_runner.util.plugins import QHAnaPluginBase, plugin_identifier
 
-_plugin_name = "hello-world"
+_plugin_name = "file-upload"
 __version__ = "v0.1.0"
 _identifier = plugin_identifier(_plugin_name, __version__)
 
 
-HELLO_BLP = SecurityBlueprint(
+FILE_UPLOAD_BLP = SecurityBlueprint(
     _identifier,  # blueprint name
     __name__,  # module import name!
-    description="Demo plugin API.",
-    template_folder="hello_world_templates",
+    description="File upload plugin API.",
+    template_folder="file_upload_templates",
 )
 
 
-class DemoResponseSchema(MaBaseSchema):
-    name = ma.fields.String(required=True, allow_none=False, dump_only=True)
-    version = ma.fields.String(required=True, allow_none=False, dump_only=True)
-    identifier = ma.fields.String(required=True, allow_none=False, dump_only=True)
-
-
-class TaskResponseSchema(MaBaseSchema):
+class TaskResponseSchema(MaBaseSchema):  # TODO: move to plugin runner?
     name = ma.fields.String(required=True, allow_none=False, dump_only=True)
     task_id = ma.fields.String(required=True, allow_none=False, dump_only=True)
     task_result_url = ma.fields.Url(required=True, allow_none=False, dump_only=True)
 
 
-class HelloWorldParametersSchema(FrontendFormBaseSchema):
-    input_str = ma.fields.String(
+class FileUploadParametersSchema(FrontendFormBaseSchema):
+    data_type = ma.fields.String(
+        required=True,
+        allow_none=False,
+        metadata={"label": "Data Type", "description": "Semantic of the data"},
+    )
+    content_type = ma.fields.String(
         required=True,
         allow_none=False,
         metadata={
-            "label": "Input String",
-            "description": "A simple string input.",
-            "input_type": "textarea",
+            "label": "Content Type",
+            "description": "Encoding of the data (mimetype)",
         },
     )
 
 
-@HELLO_BLP.route("/")
+@FILE_UPLOAD_BLP.route("/")
 class PluginsView(MethodView):
     """Plugins collection resource."""
 
-    @HELLO_BLP.response(HTTPStatus.OK, PluginMetadataSchema())
-    @HELLO_BLP.require_jwt("jwt", optional=True)
+    @FILE_UPLOAD_BLP.response(HTTPStatus.OK, PluginMetadataSchema())
+    @FILE_UPLOAD_BLP.require_jwt("jwt", optional=True)
     def get(self):
         """Endpoint returning the plugin metadata."""
-        plugin = HelloWorld.instance
+        plugin = FileUpload.instance
         if plugin is None:
             abort(HTTPStatus.INTERNAL_SERVER_ERROR)
         return PluginMetadata(
             title=plugin.name,
-            description=HELLO_BLP.description,
+            description=FILE_UPLOAD_BLP.description,
             name=plugin.identifier,
             version=plugin.version,
             type=PluginType.processing,
             entry_point=EntryPoint(
-                href=url_for(f"{HELLO_BLP.name}.ProcessView"),
-                ui_href=url_for(f"{HELLO_BLP.name}.MicroFrontend"),
+                href=url_for(f"{FILE_UPLOAD_BLP.name}.ProcessView"),
+                ui_href=url_for(f"{FILE_UPLOAD_BLP.name}.MicroFrontend"),
                 plugin_dependencies=[],
                 data_input=[],
                 data_output=[
                     DataMetadata(
-                        data_type="txt",
-                        content_type=["text/plain"],
+                        data_type="*",
+                        content_type=["*"],
                         required=True,
                     )
                 ],
@@ -118,7 +116,7 @@ class PluginsView(MethodView):
         )
 
 
-@HELLO_BLP.route("/ui/")
+@FILE_UPLOAD_BLP.route("/ui/")
 class MicroFrontend(MethodView):
     """Micro frontend for the hello world plugin."""
 
@@ -126,64 +124,66 @@ class MicroFrontend(MethodView):
         "inputStr": "Sample input string.",
     }
 
-    @HELLO_BLP.html_response(
+    @FILE_UPLOAD_BLP.html_response(
         HTTPStatus.OK, description="Micro frontend of the hello world plugin."
     )
-    @HELLO_BLP.arguments(
-        HelloWorldParametersSchema(
+    @FILE_UPLOAD_BLP.arguments(
+        FileUploadParametersSchema(
             partial=True, unknown=EXCLUDE, validate_errors_as_result=True
         ),
         location="query",
         required=False,
     )
-    @HELLO_BLP.require_jwt("jwt", optional=True)
+    @FILE_UPLOAD_BLP.require_jwt("jwt", optional=True)
     def get(self, errors):
         """Return the micro frontend."""
         return self.render(request.args, errors)
 
-    @HELLO_BLP.html_response(
+    @FILE_UPLOAD_BLP.html_response(
         HTTPStatus.OK, description="Micro frontend of the hello world plugin."
     )
-    @HELLO_BLP.arguments(
-        HelloWorldParametersSchema(
+    @FILE_UPLOAD_BLP.arguments(
+        FileUploadParametersSchema(
             partial=True, unknown=EXCLUDE, validate_errors_as_result=True
         ),
         location="form",
         required=False,
     )
-    @HELLO_BLP.require_jwt("jwt", optional=True)
+    @FILE_UPLOAD_BLP.require_jwt("jwt", optional=True)
     def post(self, errors):
         """Return the micro frontend with prerendered inputs."""
         return self.render(request.form, errors)
 
     def render(self, data: Mapping, errors: dict):
-        plugin = HelloWorld.instance
+        plugin = FileUpload.instance
         if plugin is None:
             abort(HTTPStatus.INTERNAL_SERVER_ERROR)
-        schema = HelloWorldParametersSchema()
+        schema = FileUploadParametersSchema()
         return Response(
             render_template(
-                "simple_template.html",
+                "file_upload_template.html",
                 name=plugin.name,
                 version=plugin.version,
                 schema=schema,
                 values=data,
                 errors=errors,
-                process=url_for(f"{HELLO_BLP.name}.ProcessView"),
+                process=url_for(f"{FILE_UPLOAD_BLP.name}.ProcessView"),
                 example_values=url_for(
-                    f"{HELLO_BLP.name}.MicroFrontend", **self.example_inputs
+                    f"{FILE_UPLOAD_BLP.name}.MicroFrontend", **self.example_inputs
                 ),
             )
         )
 
 
-@HELLO_BLP.route("/process/")
+@FILE_UPLOAD_BLP.route("/process/")
 class ProcessView(MethodView):
     """Start a long running processing task."""
 
-    @HELLO_BLP.arguments(HelloWorldParametersSchema(unknown=EXCLUDE), location="form")
-    @HELLO_BLP.response(HTTPStatus.OK, TaskResponseSchema())
-    @HELLO_BLP.require_jwt("jwt", optional=True)
+    @FILE_UPLOAD_BLP.arguments(
+        FileUploadParametersSchema(unknown=EXCLUDE), location="form"
+    )
+    @FILE_UPLOAD_BLP.response(HTTPStatus.OK, TaskResponseSchema())
+    @FILE_UPLOAD_BLP.require_jwt("jwt", optional=True)
     def post(self, arguments):
         """Start the demo task."""
         db_task = ProcessingTask(task_name=demo_task.name, parameters=dumps(arguments))
@@ -193,6 +193,26 @@ class ProcessView(MethodView):
         task: chain = demo_task.s(db_id=db_task.id) | save_task_result.s(db_id=db_task.id)
         # save errors to db
         task.link_error(save_task_error.s(db_id=db_task.id))
+
+        if "file" not in request.files:
+            raise ValueError("No file in request.")
+
+        file = request.files["file"]
+
+        if file.filename == "":
+            raise ValueError("No file selected.")
+
+        if file:
+            filename = secure_filename(file.filename)
+
+            STORE.persist_task_result(
+                db_task.id,
+                file.stream,
+                filename,
+                arguments["data_type"],
+                arguments["content_type"],
+            )
+
         task.apply_async()
 
         db_task.save(commit=True)
@@ -202,7 +222,7 @@ class ProcessView(MethodView):
         )
 
 
-class HelloWorld(QHAnaPluginBase):
+class FileUpload(QHAnaPluginBase):
 
     name = _plugin_name
     version = __version__
@@ -211,13 +231,13 @@ class HelloWorld(QHAnaPluginBase):
         super().__init__(app)
 
     def get_api_blueprint(self):
-        return HELLO_BLP
+        return FILE_UPLOAD_BLP
 
 
 TASK_LOGGER = get_task_logger(__name__)
 
 
-@CELERY.task(name=f"{HelloWorld.instance.identifier}.demo_task", bind=True)
+@CELERY.task(name=f"{FileUpload.instance.identifier}.demo_task", bind=True)
 def demo_task(self, db_id: int) -> str:
     TASK_LOGGER.info(f"Starting new demo task with db id '{db_id}'")
     task_data: Optional[ProcessingTask] = ProcessingTask.get_by_id(id_=db_id)
@@ -227,16 +247,4 @@ def demo_task(self, db_id: int) -> str:
         TASK_LOGGER.error(msg)
         raise KeyError(msg)
 
-    input_str: Optional[str] = loads(task_data.parameters or "{}").get("input_str", None)
-    TASK_LOGGER.info(f"Loaded input parameters from db: input_str='{input_str}'")
-    if input_str is None:
-        raise ValueError("No input argument provided!")
-    if input_str:
-        out_str = input_str.replace("input", "output")
-        with SpooledTemporaryFile(mode="w") as output:
-            output.write(out_str)
-            STORE.persist_task_result(
-                db_id, output, "out.txt", "hello-world-output", "text/plain"
-            )
-        return "result: " + repr(out_str)
-    return "Empty input string, no output could be generated!"
+    return "Done"
