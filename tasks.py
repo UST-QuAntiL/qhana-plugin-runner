38727e551b6c2e8567b555bcbbf9666f59efb0ae
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2022-04-27 16:30:01 +0200

    Remove unused import

--- tasks.py
+++ tasks.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from distutils.log import warn
 from os import environ
 from os import execvpe as replace_process
 from os import urandom
