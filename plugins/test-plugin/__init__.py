eeb9c38082d49c27345764d69eb5c9922e428caa
Philipp Wundrack <philipp.wundrack@live.de>
2022-06-21 12:02:11 +0200

    add new multistep plugin as scaffold for the optimization coordinator plugin

---
+++ plugins/test-plugin/__init__.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+from flask import Flask
+
+from qhana_plugin_runner.api.util import SecurityBlueprint
+from qhana_plugin_runner.util.plugins import plugin_identifier, QHAnaPluginBase
+
+_plugin_name = "test-plugin"
+__version__ = "v0.1.0"
+_identifier = plugin_identifier(_plugin_name, __version__)
+
+
+TEST_BLP = SecurityBlueprint(
+    _identifier,  # blueprint name
+    __name__,  # module import name!
+    description="Demo plugin API.",
+    template_folder="hello_world_templates",
+)
+
+
+class Test(QHAnaPluginBase):
+
+    name = _plugin_name
+    version = __version__
+
+    def __init__(self, app: Optional[Flask]) -> None:
+        super().__init__(app)
+
+    def get_api_blueprint(self):
+        return TEST_BLP
+
+
+try:
+    # It is important to import the routes **after** COSTUME_LOADER_BLP and CostumeLoader are defined, because they are
+    # accessed as soon as the routes are imported.
+    from . import routes
+except ImportError:
+    # When running `poetry run flask install`, importing the routes will fail, because the dependencies are not
+    # installed yet.
+    pass
