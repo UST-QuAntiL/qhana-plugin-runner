921a444b03b6fc15ddceb77f77c5c7e7e60822a7
Philipp Wundrack <philipp.wundrack@live.de>
2022-07-29 13:23:15 +0200

    add copyright and permission notice from SQLAlchemy

--- qhana_plugin_runner/db/models/mutable_json.py
+++ qhana_plugin_runner/db/models/mutable_json.py
@@ -28,7 +28,30 @@ class NestedMutable(Mutable):
     def _listen_on_attribute(cls, attribute, coerce, parent_cls):
         """Overwrite method of base class in order to accept primitive (immutable) types.
 
-        Code is identical to MutableBase._listen_on_attribute apart from handling ``str``, ``float`` and ``int`` (which includes ``bool`` as ``bool`` is a subclass of ``int``)."""
+        Code is identical to the method MutableBase._listen_on_attribute from SQLAlchemy apart from handling
+        ``str``, ``float`` and ``int`` (which includes ``bool`` as ``bool`` is a subclass of ``int``).
+
+        SQLAlchemy is licensed under the MIT license:
+
+        Copyright 2005-2022 SQLAlchemy authors and contributors <see AUTHORS file>.
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy of
+        this software and associated documentation files (the "Software"), to deal in
+        the Software without restriction, including without limitation the rights to
+        use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+        of the Software, and to permit persons to whom the Software is furnished to do
+        so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE."""
         key = attribute.key
         if parent_cls is not attribute.class_:
             return
