70a61e3bd82d6d8c76b76cd5a48b47e9e03f6759
OliviaOdenthal <olivia.odenthal@yahoo.de>
2022-08-11 15:08:41 +0200

    classical and quantum SVM basic setup (setting boolean in UI doesn't work yet)

--- plugins/svm.py
+++ plugins/svm.py
@@ -4,7 +4,7 @@ from typing import Optional, Mapping
 from json import dumps
 
 import marshmallow as ma
-from marshmallow import EXCLUDE
+from marshmallow import EXCLUDE, post_load
 from flask import abort, redirect
 from flask.app import Flask
 from flask.views import MethodView
@@ -58,18 +58,32 @@ class TaskResponseSchema(MaBaseSchema):
     task_result_url = ma.fields.Url(required=True, allow_none=False, dump_only=True)
 
 
+class InputParameters:
+    def __init__(
+        self,
+        # data_url: str,
+        use_quantum=True,
+    ):
+        # self.data_url = data_url
+        self.use_quantum = use_quantum
+
+
 class SVMSchema(FrontendFormBaseSchema):
-    # INPUT....
-    # print("TODO")  # TODO
-    input_str = ma.fields.String(
-        required=True,
+    use_quantum = ma.fields.Boolean(
+        required=False,
         allow_none=False,
         metadata={
-            "label": "Input String",
-            "description": "A simple string input.",
-            "input_type": "textarea",
+            "label": "use quantum",
+            "description": "whether to use quantum svm or classical svm",
+            "input_type": "checkbox",
         },
     )
+
+    print("USE QUANTUM", use_quantum)
+
+    @post_load
+    def make_input_params(self, data, **kwargs) -> InputParameters:
+        return InputParameters(**data)
 
 
 @SVM_BLP.route("/")
@@ -143,13 +157,25 @@ class MicroFrontend(MethodView):
         if plugin is None:
             abort(HTTPStatus.INTERNAL_SERVER_ERROR)
         schema = SVMSchema()
+
+        data_dict = dict(data)
+
+        # define default values
+        default_values = {
+            schema.fields["use_quantum"].data_key: True,
+        }
+
+        # overwrite default values with other values
+        default_values.update(data_dict)
+        data_dict = default_values
+
         return Response(
             render_template(
                 "simple_template.html",
                 name=plugin.name,
                 version=plugin.version,
                 schema=schema,
-                values=data,
+                values=data_dict,
                 errors=errors,
                 process=url_for(f"{SVM_BLP.name}.ProcessView"),
                 example_values=url_for(
@@ -168,7 +194,9 @@ class ProcessView(MethodView):
     @SVM_BLP.require_jwt("jwt", optional=True)
     def post(self, arguments):
         """Start the demo task."""
-        db_task = ProcessingTask(task_name=demo_task.name, parameters=dumps(arguments))
+        db_task = ProcessingTask(
+            task_name=demo_task.name, parameters=SVMSchema().dumps(arguments)
+        )
         db_task.save(commit=True)
 
         # all tasks need to know about db id to load the db entry
@@ -195,17 +223,95 @@ class SVM(QHAnaPluginBase):
     def get_api_blueprint(self):
         return SVM_BLP
 
+    def get_requirements(self) -> str:
+        return "qiskit~=0.27\nqiskit-aer~=0.10.4\nqiskit-machine-learning~=0.4.0\nscikit-learn~=0.24.2"  # sklearn and version like in other plugins
+
 
 TASK_LOGGER = get_task_logger(__name__)
 
 
+def get_classical_SVC(data, labels):
+    from sklearn.pipeline import make_pipeline
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.svm import SVC
+
+    TASK_LOGGER.info("classical supportvector machine")
+
+    csvc = make_pipeline(StandardScaler(), SVC(gamma="auto"))
+    csvc.fit(data, labels)
+
+    return csvc
+
+
+def get_quantum_SVC(data, labels):
+    # more parameters: backend, quantum kernel?, featuremap?
+
+    from qiskit import Aer
+    from qiskit_machine_learning.algorithms import QSVC
+    from qiskit.circuit.library import ZFeatureMap
+    from qiskit.utils import QuantumInstance
+    from qiskit_machine_learning.kernels.quantum_kernel import QuantumKernel
+
+    TASK_LOGGER.info("quantum supportvector machine")
+
+    backend = Aer.get_backend("qasm_simulator")
+    feature_map = ZFeatureMap(feature_dimension=2, reps=2)
+    quantum_instance = QuantumInstance(
+        backend, seed_simulator=9283712, seed_transpiler=9283712, shots=1024
+    )
+    qkernel = QuantumKernel(feature_map=feature_map, quantum_instance=quantum_instance)
+
+    qsvc = QSVC(quantum_kernel=qkernel)
+    qsvc.fit(data, labels)
+    return qsvc
+
+
 @CELERY.task(name=f"{SVM.instance.identifier}.demo_task", bind=True)
 def demo_task(self, db_id: int) -> str:
+    import numpy as np  # TODO
 
     TASK_LOGGER.info(f"Starting new demo task with db id '{db_id}'")
-    return "DONE"
+
+    task_data: Optional[ProcessingTask] = ProcessingTask.get_by_id(id_=db_id)
+
+    if task_data is None:
+        msg = f"Could not load task data with id {db_id} to read parameters!"
+        TASK_LOGGER.error(msg)
+        raise KeyError(msg)
+
+    input_params: InputParameters = SVMSchema().loads(task_data.parameters)
+
+    use_quantum = input_params.use_quantum
+    TASK_LOGGER.info(f"Loaded input parameters from db: use_quantum='{use_quantum}'")
+    # parameters
+    # CSVM:
+    #   regularization parameter C=1.0
+    #   kernel = rbf (linear, poly, rbf, sigmoid)
+    #   degree of the poly kernel = 3 (irrelevant for all other kernels)
+    # QSVC: (qiskit)
+    #   backend = QuantumBackends.aer_statevector_simulator
+    #   Ibmq_token = ""
+    #   Ibmq_custom_backend = ""
+    #   Featuremap = "ZFeatureMap" (ZFeatureMap, ZZFeatureMap, PauliFeatureMap)
+    #   Entanglement = "linear" (full, linear)
+    #   Reps = 2
+    #   Shots = 1024
+
+    data = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
+    labels = np.array([1, 1, 2, 2])
+    sample_test = [[-0.8, -1]]
+
+    svm = None
+    if use_quantum:
+        svm = get_quantum_SVC(data, labels)
+    else:
+        svm = get_classical_SVC(data, labels)
+
+    # TODO accuracy
+
+    # TODO visualize
+
+    return "DONE " + str(svm.predict(sample_test)) + str(use_quantum)
 
 
-# TODO
-# when clicking on SVM plugin in plugin runner:
-# {"code":405, "status":"Method Not Allowed"}
+# TODO use_quantum is always True..
