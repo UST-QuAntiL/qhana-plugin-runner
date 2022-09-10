30794529bc61a00bda0ec4698003ce005e7aa584
BenediktRiegel <Benedikt.Riegel@gmail.com>
2022-09-10 16:39:26 +0200

    Adapted qknn.py to support schuld qknn and simple hamming qknn. Also removed the fit method.

--- plugins/quantum_k_nearest_neighbours/backend/qknns/qknn.py
+++ plugins/quantum_k_nearest_neighbours/backend/qknns/qknn.py
@@ -1,41 +1,91 @@
 import numpy as np
 from abc import abstractmethod
 from enum import Enum
+from typing import List
+from pennylane import QuantumFunctionError
 
 
 class QkNNEnum(Enum):
     qiskit_qknn = "qiskit qknn"
+    schuld_qknn = "schuld qknn"
+    simple_hamming_qknn = "simple hamming qknn"
 
-    def get_qknn(self, train_data, labels, k, backend, shots):
+    def get_qknn(self, train_data, train_labels, k, backend, shots):
         if self == QkNNEnum.qiskit_qknn:
             from .qiskit_qknn import QiskitQkNN
-            return QiskitQkNN(train_data, labels, k, backend, shots)
+            return QiskitQkNN(train_data, train_labels, k, backend, shots)
+        if self == QkNNEnum.schuld_qknn:
+            from .schuld_hamming import SchuldQkNN
+            wires = self.check_and_get_qubits(SchuldQkNN, backend, train_data=train_data, train_labels=train_labels)
+            return SchuldQkNN(train_data, train_labels, wires[0], wires[1], wires[2], backend)
+        elif self == QkNNEnum.simple_hamming_qknn:
+            from .simpleQkNN import SimpleHammingQkNN
+            wires = self.check_and_get_qubits(SimpleHammingQkNN, backend, train_data=train_data)
+            return SimpleHammingQkNN(train_data, train_labels, k, wires[0], wires[1], backend)
+
+    def check_and_get_qubits(self, qknn_class, backend, **kwargs):
+        num_necessary_wires = qknn_class.get_necessary_wires(**kwargs)
+        num_total_wires = 0
+        wires = []
+        for num_wires in num_necessary_wires:
+            wires.append(list(range(num_total_wires, num_total_wires + num_wires)))
+            num_total_wires += num_wires
+        if num_total_wires > backend.num_wires:
+            raise ValueError(
+                "The quantum circuit needs at least " + str(num_total_wires)
+                + " qubits, but it only got " + str(backend.num_wires) + "!"
+            )
+        return wires
 
     def get_preferred_backend(self):
         if self == QkNNEnum.qiskit_qknn:
             return "qiskit"
         else:
             return "pennylane"
+        # return "pennylane"
 
 
 class QkNN:
 
     def __init__(self, train_data, train_labels, k, backend, shots):
+        if not isinstance(train_data, np.ndarray):
+            train_data = np.array(train_data, dtype=int)
         self.train_data = np.array(train_data)
         self.train_labels = np.array(train_labels)
-        self.k = k
+        self.unique_labels = list(set(self.train_labels))
+
+        self.k = min(k, len(self.train_data))
         self.backend = backend
         self.shots = shots
-        self.fit()
 
     @abstractmethod
-    def fit(self):
+    def label_point(self, X) -> int:
         """
-        Abstract method to fit the qknn, if necessary
+        Takes new point x as input and returns a label for it.
         """
 
-    @abstractmethod
-    def label_points(self, X):
-        """
-        Abstract method to label points
-        """
+    def label_points(self, X) -> List[int]:
+        new_labels = []
+        for x in X:
+            new_labels.append(self.label_point(x))
+        return new_labels
+
+    def check_wires(self, wire_types):
+        for idx1 in range(len(wire_types)):
+            wire_type1 = wire_types[idx1]
+            wires1 = getattr(self, wire_type1 + '_wires')
+            for idx2 in range(idx1 + 1, len(wire_types)):
+                wire_type2 = wire_types[idx2]
+                wires2 = getattr(self, wire_type2 + '_wires')
+                if any(wire in wires1 for wire in wires2):
+                    raise QuantumFunctionError(
+                        f"The {wire_type1} wires must be different from the {wire_type2} wires"
+                    )
+
+    def check_num_wires(self, wire_types, num_wires, error_msgs):
+        for i in range(len(wire_types)):
+            wire_type = wire_types[i]
+            wires = getattr(self, wire_type + '_wires')
+            if len(wires) < num_wires[i]:
+                error = f"The number of {wire_type} wires has to be greater or equal to {error_msgs[i]}"
+                raise QuantumFunctionError(error)
