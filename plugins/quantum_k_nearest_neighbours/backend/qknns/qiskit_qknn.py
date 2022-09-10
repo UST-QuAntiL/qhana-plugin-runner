253c3f13271c349b24b4f2c89717cb3b07cc942b
BenediktRiegel <Benedikt.Riegel@gmail.com>
2022-09-10 16:40:04 +0200

    Removed the fit method in qiskit_qknn

--- plugins/quantum_k_nearest_neighbours/backend/qknns/qiskit_qknn.py
+++ plugins/quantum_k_nearest_neighbours/backend/qknns/qiskit_qknn.py
@@ -6,26 +6,25 @@ import numpy as np
 
 
 class QiskitQkNN(QkNN):
-
     def __init__(self, train_data, train_labels, k, backend, shots):
-        super(QiskitQkNN).__init__(train_data, train_labels, k, backend, shots)
+        super(QiskitQkNN, self).__init__(train_data, train_labels, k, backend, shots)
+
         instance = aqua.QuantumInstance(backend, shots=self.shots)
         self.qknn = QKNeighborsClassifier(
             n_neighbors=self.k,
             quantum_instance=instance
         )
-
-    def fit(self):
         n_variables = int(np.power(2, np.ceil(np.log2(self.train_data.shape[1]))))  # should be positive power of 2
 
         # encode data
         if self.train_data.shape[1] < n_variables:
-            self.train_data = np.pad(self.train_data, [(0, 0), (0, n_variables - self.train_data.shape[1])], mode='constant',
-                                constant_values=0)
+            self.train_data = np.pad(self.train_data, [(0, 0), (0, n_variables - self.train_data.shape[1])],
+                                     mode='constant',
+                                     constant_values=0)
 
         encoded_train_data = analog.encode(self.train_data)
 
-        self.qknn.fit(encoded_train_data, self.train_labels)
+        self.train_data = encoded_train_data
 
     def label_points(self, X):
         X = np.pad(X, [(0, 0), (0, self.train_data.shape[1] - X.shape[1])], mode='constant', constant_values=0)
