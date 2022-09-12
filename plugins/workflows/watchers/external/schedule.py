9f2ce80cbb8a790cc5dd43c8aabd73ab461668a5
Philipp Wundrack <philipp.wundrack@live.de>
2022-09-12 14:49:01 +0200

    remove unused code

--- plugins/workflows/watchers/external/schedule.py
+++ plugins/workflows/watchers/external/schedule.py
@@ -3,16 +3,8 @@ from qhana_plugin_runner.celery import CELERY
 from .external_task_watcher import camunda_task_watcher
 from ... import Workflows
 
-CELERY.add_periodic_task( # FIXME is this the right place??
-    Workflows.instance.config["polling_rates"]["external_watcher"],
-    camunda_task_watcher.s(),
-    name="periodic task workflows watcher",
-)
 
-print("\n\n\n", CELERY.conf.humanize(with_defaults=False, censored=True), "\n\n\n") # TODO remove
-
-
-@CELERY.on_after_finalize.connect  # FIXME signal already fired???
+@CELERY.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
     """
     Setup periodic tasks for the camunda external task watcher. This will create a new entry in the beat scheduler.
