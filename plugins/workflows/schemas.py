716219f75190eda540fd2cf1b66c5dc1822e745d
Philipp Wundrack <philipp.wundrack@live.de>
2022-09-14 10:10:29 +0200

    refactor AnyInputSchema

--- plugins/workflows/schemas.py
+++ plugins/workflows/schemas.py
@@ -1,5 +1,6 @@
 import enum
 from collections import OrderedDict
+from typing import Dict
 
 import marshmallow as ma
 from marshmallow import INCLUDE, post_load
@@ -52,7 +53,6 @@ class WorkflowsParametersSchema(FrontendFormBaseSchema):
         return InputParameters(**data)
 
 
-# TODO: Cleanup
 class AnyInputSchema(FrontendFormBaseSchema):
     def __init__(self, params=None):
         super().__init__(unknown=INCLUDE)
@@ -60,103 +60,105 @@ class AnyInputSchema(FrontendFormBaseSchema):
         if params is None:
             params = {}
 
-        inputs = {}
+        self.inputs: Dict = {}
 
-        prefix_choices = config["qhana_input"]["prefix_value_choice"]
-        prefix_enum = config["qhana_input"]["prefix_value_enum"]
-        prefix_file_url = config["qhana_input"]["prefix_value_file_url"]
-        prefix_delimiter = config["qhana_input"]["prefix_value_delimiter"]
+        self.prefix_choices = config["qhana_input"]["prefix_value_choice"]
+        self.prefix_enum = config["qhana_input"]["prefix_value_enum"]
+        self.prefix_file_url = config["qhana_input"]["prefix_value_file_url"]
+        self.prefix_delimiter = config["qhana_input"]["prefix_value_delimiter"]
 
         for key, val in params.items():
-
             if not val["value"]:
-                inputs[key] = ma.fields.String(
-                    required=True,
-                    allow_none=False,
-                    metadata={
-                        "label": f"{key}",
-                        "description": f"Workflow Input {key}",
-                        "input_type": "text",
-                    },
-                )
-                self.__setattr__(key, inputs[key])
-                continue
-            if val["type"] == "String" and val["value"].startswith(
-                f"{prefix_choices}{prefix_delimiter}"
-            ):
-                choices = (
-                    val["value"][
-                        len(prefix_choices) + len(prefix_delimiter) : len(val["value"])
-                    ]
-                ).split(",")
-                choices_dict = {}
-                for choice in choices:
-                    choices_dict[choice] = choice
-                choices_enum = enum.Enum("Enum", choices_dict)
-                inputs[key] = EnumField(
-                    choices_enum,
-                    required=True,
-                    allow_none=False,
-                    metadata={
-                        "label": f"{key}",
-                        "description": f"Workflow Input {key}",
-                        "input_type": "select",
-                    },
-                )
-                self.__setattr__(key, inputs[key])
-            elif val["type"] == "String" and val["value"].startswith(
-                f"{prefix_enum}{prefix_delimiter}"
-            ):
-                choices = (
-                    val["value"][
-                        len(prefix_enum) + len(prefix_delimiter) : len(val["value"])
-                    ]
-                ).split(",")
-                choices_dict = {}
-                for choice in choices:
-                    enum_key, enum_val = choice.split(":")
-                    choices_dict[enum_key.strip()] = enum_val.strip()
-                choices_enum = enum.Enum("Enum", choices_dict)
-                inputs[key] = EnumField(
-                    choices_enum,
-                    required=True,
-                    allow_none=False,
-                    metadata={
-                        "label": f"{key}",
-                        "description": f"Workflow Input {key}",
-                        "input_type": "select",
-                    },
-                )
-                self.__setattr__(key, inputs[key])
-            elif val["type"] == "String" and val["value"].startswith(
-                f"{prefix_file_url}{prefix_delimiter}"
-            ):
-                data_input_type, data_content_types = (
-                    val["value"][
-                        len(prefix_file_url) + len(prefix_delimiter) : len(val["value"])
-                    ]
-                ).split(",")
-                inputs[key] = FileUrl(
-                    required=True,
-                    allow_none=False,
-                    data_input_type=data_input_type,
-                    data_content_types=data_content_types,
-                    metadata={
-                        "label": f"{key}",
-                        "description": f"Workflow Input {key}",
-                        "input_type": "text",
-                    },
-                )
+                self.add_string_field(key)
             elif val["type"] == "String":
-                inputs[key] = ma.fields.String(
-                    required=True,
-                    allow_none=False,
-                    metadata={
-                        "label": f"{key}",
-                        "description": f"Workflow Input {key}",
-                        "input_type": "text",
-                    },
-                )
-                self.__setattr__(key, inputs[key])
+                if val["value"].startswith(
+                    f"{self.prefix_choices}{self.prefix_delimiter}"
+                ):
+                    self.add_choices_field(val, key)
+                elif val["value"].startswith(
+                    f"{self.prefix_enum}{self.prefix_delimiter}"
+                ):
+                    self.add_enum_field(val, key)
+                elif val["value"].startswith(
+                    f"{self.prefix_file_url}{self.prefix_delimiter}"
+                ):
+                    self.add_file_url_field(val, key)
+                else:
+                    self.add_string_field(key)
 
-        self.fields = OrderedDict(inputs)
+        self.fields = OrderedDict(self.inputs)
+
+    def add_string_field(self, key: str):
+        self.inputs[key] = ma.fields.String(
+            required=True,
+            allow_none=False,
+            metadata={
+                "label": f"{key}",
+                "description": f"Workflow Input {key}",
+                "input_type": "text",
+            },
+        )
+        self.__setattr__(key, self.inputs[key])
+
+    def add_choices_field(self, val: Dict, key: str):
+        choices = (
+            val["value"][
+                len(self.prefix_choices) + len(self.prefix_delimiter) : len(val["value"])
+            ]
+        ).split(",")
+        choices_dict = {}
+        for choice in choices:
+            choices_dict[choice] = choice
+        choices_enum = enum.Enum("Enum", choices_dict)
+        self.inputs[key] = EnumField(
+            choices_enum,
+            required=True,
+            allow_none=False,
+            metadata={
+                "label": f"{key}",
+                "description": f"Workflow Input {key}",
+                "input_type": "select",
+            },
+        )
+        self.__setattr__(key, self.inputs[key])
+
+    def add_enum_field(self, val: Dict, key: str):
+        choices = (
+            val["value"][
+                len(self.prefix_enum) + len(self.prefix_delimiter) : len(val["value"])
+            ]
+        ).split(",")
+        choices_dict = {}
+        for choice in choices:
+            enum_key, enum_val = choice.split(":")
+            choices_dict[enum_key.strip()] = enum_val.strip()
+        choices_enum = enum.Enum("Enum", choices_dict)
+        self.inputs[key] = EnumField(
+            choices_enum,
+            required=True,
+            allow_none=False,
+            metadata={
+                "label": f"{key}",
+                "description": f"Workflow Input {key}",
+                "input_type": "select",
+            },
+        )
+        self.__setattr__(key, self.inputs[key])
+
+    def add_file_url_field(self, val: Dict, key: str):
+        data_input_type, data_content_types = (
+            val["value"][
+                len(self.prefix_file_url) + len(self.prefix_delimiter) : len(val["value"])
+            ]
+        ).split(",")
+        self.inputs[key] = FileUrl(
+            required=True,
+            allow_none=False,
+            data_input_type=data_input_type,
+            data_content_types=data_content_types,
+            metadata={
+                "label": f"{key}",
+                "description": f"Workflow Input {key}",
+                "input_type": "text",
+            },
+        )
