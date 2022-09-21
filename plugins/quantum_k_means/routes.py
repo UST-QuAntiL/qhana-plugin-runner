cf3627a53ed052e70e4de9afe5e49375efeb2d1f
BenediktRiegel <Benedikt.Riegel@gmail.com>
2022-09-21 11:23:00 +0200

    Removed unused description variable in quantum_k_means/routes.py

--- plugins/quantum_k_means/routes.py
+++ plugins/quantum_k_means/routes.py
@@ -41,14 +41,6 @@ from qhana_plugin_runner.tasks import save_task_error, save_task_result
 from .tasks import calculation_task
 
 
-description = "This plugin groups the data into different clusters, with the help of quantum algorithms.\n" \
-              "Currently there are four implemented algorithms. Destructive interference and negative rotation are from [0], " \
-              "positive correlation is from [1] and state preparation is from a previous colleague.\n\n" \
-              "Source:\n" \
-              "[0] S. Khan and A. Awan and G. Vall-Llosera. K-Means Clustering on Noisy Intermediate Scale Quantum Computers.arXiv. <a href=\"https://doi.org/10.48550/ARXIV.1909.12183\">https://doi.org/10.48550/ARXIV.1909.12183</a>\n" \
-              "[1] https://towardsdatascience.com/quantum-machine-learning-distance-estimation-for-k-means-clustering-26bccfbfcc76"
-
-
 @QKMEANS_BLP.route("/")
 class PluginsView(MethodView):
     """Plugins collection resource."""
