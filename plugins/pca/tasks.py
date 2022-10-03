0376dbe616b72548990f8c314499a88fb014ba7f
BenediktRiegel <Benedikt.Riegel@gmail.com>
2022-10-03 10:42:18 +0200

    pca: add an empty line for more readability

--- plugins/pca/tasks.py
+++ plugins/pca/tasks.py
@@ -174,6 +174,7 @@ def get_pca(input_params: ParameterHandler):
         eigen_solver = input_params.get("solver")
         if eigen_solver == "full":
             eigen_solver = "dense"
+
         return KernelPCA(
             n_components=input_params.get("dimensions"),
             kernel=input_params.get("kernel"),
