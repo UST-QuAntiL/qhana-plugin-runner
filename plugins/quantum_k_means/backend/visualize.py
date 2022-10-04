fe0bd0715fdab64acce90eddb41eee53ca3eb177
BenediktRiegel <Benedikt.Riegel@gmail.com>
2022-10-04 20:46:02 +0200

    QkMeans: Cleaned up visualize a bit. Also changed case 'dim == 3' to 'dim >= 3'

--- plugins/quantum_k_means/backend/visualize.py
+++ plugins/quantum_k_means/backend/visualize.py
@@ -14,41 +14,30 @@
 
 import pandas as pd
 import plotly.express as px
+import numpy as np
 
 
 def plot_data(data_points, clusters, only_first_100=True):
+    data_points = data_points[:100]
     dim = len(data_points[0]["point"])
     cluster_dict = {}
     for cluster_entry in clusters:
         cluster_dict[cluster_entry["ID"]] = int(cluster_entry["cluster"])
-    if dim == 3:
-        points_x = []
-        points_y = []
-        points_z = []
-        ids = []
-        cluster = []
-        if only_first_100:
-            for i in range(min(len(data_points), 100)):
-                entity = data_points[i]
-                point = entity["point"]
-                points_x.append(float(point[0]))
-                points_y.append(float(point[1]))
-                points_z.append(float(point[2]))
-                ids.append(entity["ID"])
-                cluster.append(cluster_dict[entity["ID"]])
-        else:
-            for entity in data_points:
-                point = entity["point"]
-                points_x.append(float(point[0]))
-                points_y.append(float(point[1]))
-                points_z.append(float(point[2]))
-                ids.append(entity["ID"])
-                cluster.append(cluster_dict[entity["ID"]])
+
+    points = np.empty((len(data_points), dim))
+    ids = []
+    cluster = []
+    for i, entity in enumerate(data_points):
+        points[i] = np.array(entity["point"], dtype=float)
+        ids.append(entity["ID"])
+        cluster.append(cluster_dict[entity["ID"]])
+
+    if dim >= 3:
         df = pd.DataFrame(
             {
-                "x": points_x,
-                "y": points_y,
-                "z": points_z,
+                "x": points[:, 0],
+                "y": points[:, 1],
+                "z": points[:, 2],
                 "ID": ids,
                 "size": [10] * len(ids),
                 "cluster": cluster,
@@ -64,65 +53,15 @@ def plot_data(data_points, clusters, only_first_100=True):
             color="cluster",
             symbol="cluster",
         )
-    elif dim == 2:
-        points_x = []
-        points_y = []
-        ids = []
-        cluster = []
-        if only_first_100:
-            for i in range(min(len(data_points), 100)):
-                entity = data_points[i]
-                point = entity["point"]
-                points_x.append(float(point[0]))
-                points_y.append(float(point[1]))
-                ids.append(entity["ID"])
-                cluster.append(cluster_dict[entity["ID"]])
-        else:
-            for entity in data_points:
-                point = entity["point"]
-                points_x.append(float(point[0]))
-                points_y.append(float(point[1]))
-                ids.append(entity["ID"])
-                cluster.append(cluster_dict[entity["ID"]])
-        df = pd.DataFrame(
-            {
-                "x": points_x,
-                "y": points_y,
-                "ID": ids,
-                "size": [10] * len(ids),
-                "cluster": cluster,
-            }
-        )
-        return px.scatter(
-            df,
-            x="x",
-            y="y",
-            hover_name="ID",
-            size="size",
-            color="cluster",
-            symbol="cluster",
-        )
     else:
-        points_x = []
-        ids = []
-        cluster = []
-        if only_first_100:
-            for i in range(min(len(data_points), 100)):
-                entity = data_points[i]
-                point = entity["point"]
-                points_x.append(float(point[0]))
-                ids.append(entity["ID"])
-                cluster.append(cluster_dict[entity["ID"]])
+        if dim == 2:
+            points_y = points[:, 1]
         else:
-            for entity in data_points:
-                point = entity["point"]
-                points_x.append(float(point[0]))
-                ids.append(entity["ID"])
-                cluster.append(cluster_dict[entity["ID"]])
+            points_y = [0] * len(points)
         df = pd.DataFrame(
             {
-                "x": points_x,
-                "y": [0] * len(ids),
+                "x": points[:, 0],
+                "y": points_y,
                 "ID": ids,
                 "size": [10] * len(ids),
                 "cluster": cluster,
