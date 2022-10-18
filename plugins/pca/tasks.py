3ca755a7b3cdca43ad3883c5376a30e67ed4186b
BenediktRiegel <Benedikt.Riegel@gmail.com>
2022-10-19 00:09:35 +0200

    Changed how zero paddings for the dim<int> attributes gets added.

--- plugins/pca/tasks.py
+++ plugins/pca/tasks.py
@@ -202,7 +202,7 @@ def get_dim_attributes(dim):
     # for each dimension we have an attribute, i.e. dimension 0 = dim0, dimension 1 = dim1, ...
     # Using the zero padding, we ensure that every dim<int> has the same length, e.g. dim00, dim01, ..., dim10, dim11
     zero_padding = len(str(dim - 1))
-    dim_attributes = [f"dim{str(d).rjust(zero_padding, '0')}" for d in range(dim)]
+    dim_attributes = [f"dim{d:0{zero_padding}}" for d in range(dim)]
     return dim_attributes
 
 
