7c2846112c2a695e098786c8922fe7015eb6aa31
BenediktRiegel <Benedikt.Riegel@gmail.com>
2022-11-02 10:48:36 +0100

    pca_ouput: Fixed return types and using singledispatch now

--- plugins/pca/pca_output.py
+++ plugins/pca/pca_output.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 from sklearn.decomposition import PCA, KernelPCA, IncrementalPCA, SparsePCA
-from sklearn.base import BaseEstimator
+from functools import singledispatch
 
 from celery.utils.log import get_task_logger
 
@@ -25,8 +25,35 @@ This file prepares a dictionary with all the necessary parameters of the PCA, to
 In other scikit-learn versions the value names could change!
 """
 
+# dim = num features of output. Return dim here, since input params of the plugin can be <= 0
+def get_output_dimensionality(pca):
+    """
+    This method takes a fitted pca by sklearn as an input and returns the dimensionality of a point after transformation.
+    :param pca: one of sklearns pca types, e.g. PCA or KernelPCA
+    :return: int an integer equal to the output's number of dimensions
+    """
+    if type(pca) == KernelPCA:
+        return len(pca.eigenvectors_[0])
+    else:
+        # dimensions for normal, incremental and sparse pca
+        return pca.n_components_
 
-def pca_output(pca: PCA) -> (dict, int):
+
+@singledispatch
+def pca_to_output(pca) -> dict:
+    """
+    This method takes a fitted pca by sklearn as an input and converts its state into a dictionary inorder to save it.
+    Additionally, it returns the dimensionality of a transformed data point.
+    :param pca: one of sklearns pca types, e.g. PCA or KernelPCA
+    :return: (dict, int) a dictionary to save the current state of the PCA and an integer equal to the output's number of dimensions
+    """
+    raise ValueError(
+        f"Creating an output for object type {type(pca)} is not implemented!"
+    )
+
+
+@pca_to_output.register
+def _(pca: PCA) -> dict:
     pca_out = {
         "type": "normal",
         "components": pca.components_.tolist(),
@@ -36,7 +63,8 @@ def pca_output(pca: PCA) -> (dict, int):
     return pca_out
 
 
-def incremental_pca_output(pca: IncrementalPCA) -> (dict, int):
+@pca_to_output.register
+def _(pca: IncrementalPCA) -> dict:
     incremental_pca_out = {
         "type": "incremental",
         "components": pca.components_.tolist(),
@@ -46,7 +74,8 @@ def incremental_pca_output(pca: IncrementalPCA) -> (dict, int):
     return incremental_pca_out
 
 
-def sparse_pca_output(pca: SparsePCA) -> (dict, int):
+@pca_to_output.register
+def _(pca: SparsePCA) -> dict:
     sparse_pca_out = {
         "type": "sparse",
         "components": pca.components_.tolist(),
@@ -55,8 +84,8 @@ def sparse_pca_output(pca: SparsePCA) -> (dict, int):
     }
     return sparse_pca_out
 
-
-def kernel_pca_output(pca: KernelPCA) -> (dict, int):
+@pca_to_output.register
+def _(pca: KernelPCA) -> dict:
     kernel_pca_out = {
         "type": "kernel",
         "eigenvalues": pca.eigenvalues_.tolist(),
@@ -74,38 +103,3 @@ def kernel_pca_output(pca: KernelPCA) -> (dict, int):
     kernel_pca_out["degree"] = pca.degree
     kernel_pca_out["coef0"] = pca.coef0
     return kernel_pca_out
-
-
-# dim = num features of output. Return dim here, since input params of the plugin can be <= 0
-def pca_to_output(pca: BaseEstimator) -> dict:
-    """
-    This method takes a fitted pca by sklearn as an input and converts its state into a dictionary inorder to save it.
-    Additionally, it returns the dimensionality of a transformed data point.
-    :param pca: one of sklearns pca types, e.g. PCA or KernelPCA
-    :return: (dict, int) a dictionary to save the current state of the PCA and an integer equal to the output's number of dimensions
-    """
-    if type(pca) == PCA:
-        return pca_output(pca)
-    elif type(pca) == IncrementalPCA:
-        return incremental_pca_output(pca)
-    elif type(pca) == SparsePCA:
-        return sparse_pca_output(pca)
-    elif type(pca) == KernelPCA:
-        return kernel_pca_output(pca)
-    else:
-        raise ValueError(
-            f"Creating an output for object type {type(pca)} is not implemented!"
-        )
-
-
-def get_output_dimensionality(pca):
-    """
-    This method takes a fitted pca by sklearn as an input and returns the dimensionality of a point after transformation.
-    :param pca: one of sklearns pca types, e.g. PCA or KernelPCA
-    :return: int an integer equal to the output's number of dimensions
-    """
-    if type(pca) == KernelPCA:
-        return len(pca.eigenvectors_[0])
-    else:
-        # dimensions for normal, incremental and sparse pca
-        return pca.n_components_
