8d1df2424fa3aef937235b84858a9cf63f64c2c1
Fabian Weik <fabian.weik@protonmail.com>
2022-11-05 13:51:17 +0100

    add process endpoint to nisq analyzer plugin

--- plugins/nisq_analyzer.py
+++ plugins/nisq_analyzer.py
@@ -13,16 +13,24 @@
 # limitations under the License.
 
 from http import HTTPStatus
-from typing import Optional
+from json import dumps, loads
+from tempfile import SpooledTemporaryFile
+from typing import Mapping, Optional
 from os import environ
 
-from flask import abort
+import marshmallow as ma
+from celery.canvas import chain
+from celery.utils.log import get_task_logger
+from flask import abort, redirect
 from flask.app import Flask
+from flask.globals import request
 from flask.helpers import url_for
 from flask.views import MethodView
+from flask.wrappers import Response
 from marshmallow import EXCLUDE
 
 from qhana_plugin_runner.api.plugin_schemas import (
+    DataMetadata,
     EntryPoint,
     PluginMetadata,
     PluginMetadataSchema,
@@ -30,8 +38,13 @@ from qhana_plugin_runner.api.plugin_schemas import (
 )
 from qhana_plugin_runner.api.util import (
     FrontendFormBaseSchema,
+    MaBaseSchema,
     SecurityBlueprint,
 )
+from qhana_plugin_runner.celery import CELERY
+from qhana_plugin_runner.db.models.tasks import ProcessingTask
+from qhana_plugin_runner.storage import STORE
+from qhana_plugin_runner.tasks import save_task_error, save_task_result
 from qhana_plugin_runner.util.plugins import QHAnaPluginBase, plugin_identifier
 
 _plugin_name = "nisq-analyzer"
@@ -44,6 +57,23 @@ NISQ_BLP = SecurityBlueprint(
     __name__,  # module import name!
     description="NISQ Analyzer Plugin.",
 )
+
+class TaskResponseSchema(MaBaseSchema):
+    name = ma.fields.String(required=True, allow_none=False, dump_only=True)
+    task_id = ma.fields.String(required=True, allow_none=False, dump_only=True)
+    task_result_url = ma.fields.Url(required=True, allow_none=False, dump_only=True)
+
+
+class NisqAnalyzerParametersSchema(FrontendFormBaseSchema):
+    input_str = ma.fields.String(
+        required=True,
+        allow_none=False,
+        metadata={
+            "label": "Input String",
+            "description": "A simple string input.",
+            "input_type": "textarea",
+        },
+    )
 
 
 @NISQ_BLP.route("/")
@@ -64,13 +94,45 @@ class PluginsView(MethodView):
             version=plugin.version,
             type=PluginType.processing,
             entry_point=EntryPoint(
-                href=url_for(f"{NISQ_BLP.name}.PluginsView"),
+                href=url_for(f"{NISQ_BLP.name}.ProcessView"),
                 ui_href=plugin.url,
                 plugin_dependencies=[],
                 data_input=[],
-                data_output=[],
+                data_output=[
+                    DataMetadata(
+                        data_type="txt",
+                        content_type=["text/plain"],
+                        required=True,
+                    )
+                ],
             ),
             tags=NisqAnalyzer.instance.tags,
+        )
+
+
+@NISQ_BLP.route("/process/")
+class ProcessView(MethodView):
+    """Start a long running processing task."""
+
+    @NISQ_BLP.arguments(NisqAnalyzerParametersSchema(unknown=EXCLUDE), location="form")
+    @NISQ_BLP.response(HTTPStatus.OK, TaskResponseSchema())
+    @NISQ_BLP.require_jwt("jwt", optional=True)
+    def post(self, arguments):
+        """Start the demo task."""
+        print('\n'*100, arguments, '\n'*100)
+        db_task = ProcessingTask(task_name=demo_task.name, parameters=dumps(arguments))
+        db_task.save(commit=True)
+
+        # all tasks need to know about db id to load the db entry
+        task: chain = demo_task.s(db_id=db_task.id) | save_task_result.s(db_id=db_task.id)
+        # save errors to db
+        task.link_error(save_task_error.s(db_id=db_task.id))
+        task.apply_async()
+
+        db_task.save(commit=True)
+
+        return redirect(
+            url_for("tasks-api.TaskView", task_id=str(db_task.id)), HTTPStatus.SEE_OTHER
         )
 
 
@@ -89,3 +151,31 @@ class NisqAnalyzer(QHAnaPluginBase):
 
     def get_api_blueprint(self):
         return NISQ_BLP
+
+
+TASK_LOGGER = get_task_logger(__name__)
+
+
+@CELERY.task(name=f"{NisqAnalyzer.instance.identifier}.demo_task", bind=True)
+def demo_task(self, db_id: int) -> str:
+    TASK_LOGGER.info(f"Starting new demo task with db id '{db_id}'")
+    task_data: Optional[ProcessingTask] = ProcessingTask.get_by_id(id_=db_id)
+
+    if task_data is None:
+        msg = f"Could not load task data with id {db_id} to read parameters!"
+        TASK_LOGGER.error(msg)
+        raise KeyError(msg)
+
+    input_str: Optional[str] = loads(task_data.parameters or "{}").get("input_str", None)
+    TASK_LOGGER.info(f"Loaded input parameters from db: input_str='{input_str}'")
+    if input_str is None:
+        raise ValueError("No input argument provided!")
+    if input_str:
+        out_str = input_str.replace("input", "output")
+        with SpooledTemporaryFile(mode="w") as output:
+            output.write(out_str)
+            STORE.persist_task_result(
+                db_id, output, "nisq_analyzer.txt", "nisq-analyzer-analysis", "text/plain"
+            )
+        return "result: " + repr(out_str)
+    return "Empty input string, no output could be generated!"
