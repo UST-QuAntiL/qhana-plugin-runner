d57e00f5dbe9e3bf06a98a85330c506586468332
BenediktRiegel <Benedikt.Riegel@gmail.com>
2022-12-14 08:56:44 +0100

    Removed redundant line

--- plugins/quantum_k_nearest_neighbours/tasks.py
+++ plugins/quantum_k_nearest_neighbours/tasks.py
@@ -171,8 +171,6 @@ def calculation_task(self, db_id: int) -> str:
 
     fig = plot_data(train_data, train_id_to_idx, train_labels, test_data, test_id_to_idx, test_labels)
     if fig is not None:
-        fig.update_layout(showlegend=True)
-
         with SpooledTemporaryFile(mode="wt") as output:
             html = fig.to_html()
             output.write(html)
