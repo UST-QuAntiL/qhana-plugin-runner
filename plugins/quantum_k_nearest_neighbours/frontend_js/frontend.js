76a447e2a85000805f167092a39f85994184c3e8
BenediktRiegel <Benedikt.Riegel@gmail.com>
2023-01-09 18:37:18 +0100

    Added copyright header

--- plugins/quantum_k_nearest_neighbours/frontend_js/frontend.js
+++ plugins/quantum_k_nearest_neighbours/frontend_js/frontend.js
@@ -1,3 +1,17 @@
+// Copyright 2023 QHAna plugin runner contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Long block of just gathering elements
 // If a variable ends with _value, it's the element itself and the value can be retrieved or set
 var qknn_type_value = document.getElementById("variant");
