b1f54e431ae634800d0c9defc83704a422ccb900
BenediktRiegel <Benedikt.Riegel@gmail.com>
2023-01-28 10:15:09 +0100

    Updated __ini__ for frontend_js

--- plugins/quantum_k_nearest_neighbours/frontend_js/__init__.py
+++ plugins/quantum_k_nearest_neighbours/frontend_js/__init__.py
@@ -12,15 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
-_file_dir = os.path.dirname(__file__)
-
-_frontend_js_content = ""
-with open(_file_dir + "/frontend.js") as f:
-    _frontend_js_content = f.read()
-    f.close()
+from pathlib import Path
 
 
 def get_frontend_js() -> str:
-    return _frontend_js_content
+    return (Path(__file__).parent/"frontend.js").read_text()
