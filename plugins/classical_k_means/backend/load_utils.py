618f2354b44b222f307b740417384b42ed1e2fca
BenediktRiegel <Benedikt.Riegel@gmail.com>
2023-03-21 23:47:21 +0100

    Added classical k means

---
+++ plugins/classical_k_means/backend/load_utils.py
@@ -0,0 +1,44 @@
+from typing import List
+import numpy as np
+from qhana_plugin_runner.plugin_utils.entity_marshalling import (
+    load_entities,
+    ensure_dict,
+)
+from qhana_plugin_runner.requests import open_url
+
+
+def get_point(ent: dict) -> np.ndarray:
+    dimension_keys = [k for k in ent.keys() if k not in ("ID", "href")]
+    dimension_keys.sort()
+    point = np.empty(len(dimension_keys))
+    for idx, d in enumerate(dimension_keys):
+        point[idx] = ent[d]
+    return point
+
+
+def get_entity_generator(entity_points_url: str):
+    """
+    Return a generator for the entity points, given an url to them.
+    :param entity_points_url: url to the entity points
+    """
+    file_ = open_url(entity_points_url)
+    file_.encoding = "utf-8"
+    file_type = file_.headers["Content-Type"]
+    entities_generator = load_entities(file_, mimetype=file_type)
+    entities_generator = ensure_dict(entities_generator)
+    for ent in entities_generator:
+        yield {"ID": ent["ID"], "href": ent.get("href", ""), "point": get_point(ent)}
+
+
+def get_indices_and_point_arr(entity_points_url: str) -> (dict, np.array):
+    entity_points = list(get_entity_generator(entity_points_url))
+    id_list = []
+    points_arr = []
+
+    for ent in entity_points:
+        if ent["ID"] in id_list:
+            raise ValueError("Duplicate ID: ", ent["ID"])
+        id_list.append(ent["ID"])
+        points_arr.append(ent["point"])
+
+    return id_list, np.array(points_arr)
---
+++ plugins/classical_k_means/tasks.py
@@ -0,0 +1,88 @@
+# Copyright 2023 QHAna plugin runner contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from tempfile import SpooledTemporaryFile
+
+from typing import Optional
+
+from celery.utils.log import get_task_logger
+
+from . import ClassicalKMeans
+
+from .schemas import (
+    InputParameters,
+    InputParametersSchema,
+)
+from qhana_plugin_runner.celery import CELERY
+from qhana_plugin_runner.db.models.tasks import ProcessingTask
+from qhana_plugin_runner.plugin_utils.entity_marshalling import (
+    save_entities,
+)
+from qhana_plugin_runner.storage import STORE
+
+from .backend.load_utils import get_indices_and_point_arr
+from sklearn.cluster import KMeans
+
+
+TASK_LOGGER = get_task_logger(__name__)
+
+
+@CELERY.task(name=f"{ClassicalKMeans.instance.identifier}.calculation_task", bind=True)
+def calculation_task(self, db_id: int) -> str:
+    # get parameters
+
+    TASK_LOGGER.info(
+        f"Starting new classical k means calculation task with db id '{db_id}'"
+    )
+    task_data: Optional[ProcessingTask] = ProcessingTask.get_by_id(id_=db_id)
+
+    if task_data is None:
+        msg = f"Could not load task data with id {db_id} to read parameters!"
+        TASK_LOGGER.error(msg)
+        raise KeyError(msg)
+
+    input_params: InputParameters = InputParametersSchema().loads(task_data.parameters)
+    
+    entity_points_url = input_params.entity_points_url
+    num_clusters = input_params.num_clusters
+    maxiter = input_params.maxiter
+    relative_residual = input_params.relative_residual
+
+    TASK_LOGGER.info(f"Loaded input parameters from db: {str(input_params)}")
+
+    id_list, points = get_indices_and_point_arr(entity_points_url)
+
+    tol = relative_residual / 100.0
+    kmeans = KMeans(
+        n_clusters=num_clusters,
+        random_state=0,
+        max_iter=maxiter,
+        tol=tol
+    )
+    labels = [{"ID": _id, "href": "", "label": int(_label)} for _id, _label in zip(id_list, kmeans.fit_predict(points))]
+    print(f"labels: {labels}")
+    
+    # Output data
+    with SpooledTemporaryFile(mode="w") as output:
+        save_entities(labels, output, "application/json")
+        STORE.persist_task_result(
+            db_id,
+            output,
+            "labels.json",
+            "entity/label",
+            "application/json",
+        )
+
+
+    return "Result stored in file"
