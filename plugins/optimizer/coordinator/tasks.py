ef35865a8c94d20b15b1aca9dbcd2baac00d7d81
Matthias Weilinger <matthias.weilinger@gmx.de>
2023-05-12 09:14:38 +0200

    reorganize routes for optimizer plugin

--- plugins/optimizer/coordinator/tasks.py
+++ plugins/optimizer/coordinator/tasks.py
@@ -14,16 +14,16 @@ TASK_LOGGER = get_task_logger(__name__)
 
 
 @CELERY.task(
-    name=f"{Optimizer.instance.identifier}.objective_function_selection", bind=True
+    name=f"{Optimizer.instance.identifier}.no_op_task", bind=True
 )
-def objective_function_selection(self, db_id: int) -> str:
+def no_op_task(self, db_id: int) -> str:
     """
-    First processing task. Retrieves the input url from the database and stores it in a file.
+    A no operation task that is run when no operation is needed.
     @param self:
     @param db_id: database ID that will be used to retrieve the task data from the database
     @return: log message
     """
-    TASK_LOGGER.info(f"Starting processing step 1 task with db id '{db_id}'")
+    TASK_LOGGER.info(f"Starting no op task with db id '{db_id}'")
     task_data: Optional[ProcessingTask] = ProcessingTask.get_by_id(id_=db_id)
 
     if task_data is None:
@@ -31,59 +31,4 @@ def objective_function_selection(self, db_id: int) -> str:
         TASK_LOGGER.error(msg)
         raise KeyError(msg)
 
-    invoked_plugin = task_data.data.get("invoked_plugin")
-    TASK_LOGGER.info(f"Loaded input parameters from db: called plugin='{invoked_plugin}'")
-
-    if invoked_plugin is None:
-        raise ValueError("No input data provided!")
-
-    out_str = "Selected objective-function plugin: " + invoked_plugin
-
-    if invoked_plugin is None:
-        raise ValueError("No objective-function plugin provided!")
-
-    # store data in file
-    with SpooledTemporaryFile(mode="w") as output:
-        output.write(out_str)
-        STORE.persist_task_result(
-            db_id,
-            output,
-            "optimizer_output_of_selection.txt",
-            "hello-world-output",
-            "text/plain",
-        )
-
-    return "result: " + repr(out_str)
-
-
-@CELERY.task(name=f"{Optimizer.instance.identifier}.dataset_selection", bind=True)
-def dataset_selection(self, db_id: int) -> str:
-    """
-    First processing task. Retrieves the input url from the database and stores it in a file.
-    @param self:
-    @param db_id: database ID that will be used to retrieve the task data from the database
-    @return: log message
-    """
-    TASK_LOGGER.info(f"Starting dataset selection task with db id '{db_id}'")
-    task_data: Optional[ProcessingTask] = ProcessingTask.get_by_id(id_=db_id)
-
-    if task_data is None:
-        msg = f"Could not load task data with id {db_id} to read parameters!"
-        TASK_LOGGER.error(msg)
-        raise KeyError(msg)
-
-    input_file_url = task_data.data.get("input_file_url")
-    out_str = "Selected dataset url: " + input_file_url
-
-    # store data in file
-    with SpooledTemporaryFile(mode="w") as output:
-        output.write(out_str)
-        STORE.persist_task_result(
-            db_id,
-            output,
-            "optimizer_output_data_selection.txt",
-            "hello-world-output",
-            "text/plain",
-        )
-
-    return "result: " + repr(out_str)
+    return ""
