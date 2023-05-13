eac9d163a8e6d900ceb6b2c2c4cec1c33747a3f5
Matthias Weilinger <matthias.weilinger@gmx.de>
2023-05-13 12:49:01 +0200

    Add ridge loss objective function plugin

---
+++ plugins/optimizer/objective_functions/ridge_loss/schemas.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+import marshmallow as ma
+from qhana_plugin_runner.api.util import (
+    FrontendFormBaseSchema,
+    MaBaseSchema,
+)
+
+
+@dataclass
+class Hyperparaters:
+    aplpha: float
+
+
+class RidgeLossTaskResponseSchema(MaBaseSchema):
+    name = ma.fields.String(required=True, allow_none=False, dump_only=True)
+    task_id = ma.fields.String(required=True, allow_none=False, dump_only=True)
+    task_result_url = ma.fields.Url(required=True, allow_none=False, dump_only=True)
+
+
+class HyperparamterInputSchema(FrontendFormBaseSchema):
+    alpha = ma.fields.Float(
+        required=True,
+        allow_none=False,
+        metadata={
+            "label": "Aplha",
+            "description": "Alpha variable for Ridge Loss function.",
+            "input_type": "textarea",
+        },
+    )
