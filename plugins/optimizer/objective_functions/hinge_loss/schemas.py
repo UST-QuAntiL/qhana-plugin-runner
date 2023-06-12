ea664d48f6de73917d06d1ff0b537dd3706c6eaf
Matthias Weilinger <matthias.weilinger@gmx.de>
2023-06-12 13:55:37 +0200

    add hinge loss of

---
+++ plugins/optimizer/objective_functions/hinge_loss/schemas.py
@@ -0,0 +1,35 @@
+# Copyright 2023 QHAna plugin runner contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import marshmallow as ma
+
+from qhana_plugin_runner.api.util import FrontendFormBaseSchema, MaBaseSchema
+
+
+class HingeLossTaskResponseSchema(MaBaseSchema):
+    name = ma.fields.String(required=True, allow_none=False, dump_only=True)
+    task_id = ma.fields.String(required=True, allow_none=False, dump_only=True)
+    task_result_url = ma.fields.Url(required=True, allow_none=False, dump_only=True)
+
+
+class HyperparamterInputSchema(FrontendFormBaseSchema):
+    c = ma.fields.Float(
+        required=True,
+        allow_none=False,
+        metadata={
+            "label": "Regularization parameter",
+            "description": "Regularization parameter for Hinge Loss function.",
+            "input_type": "textarea",
+        },
+    )
