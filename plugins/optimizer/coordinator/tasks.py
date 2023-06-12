f993a51c0d32726267105182d68db02bd39983fe
Matthias Weilinger <matthias.weilinger@gmx.de>
2023-06-12 11:51:44 +0200

    move file writer to optimizer

--- plugins/optimizer/coordinator/tasks.py
+++ plugins/optimizer/coordinator/tasks.py
@@ -1,3 +1,6 @@
+import ast
+from tempfile import SpooledTemporaryFile
+from time import sleep
 from typing import Iterator, Optional
 
 from celery.utils.log import get_task_logger
@@ -6,16 +9,43 @@ import requests
 from plugins.optimizer.coordinator.shared_schemas import (
     MinimizerInputData,
     MinimizerInputSchema,
+    MinimizerResult,
+    MinimizerResultSchema,
 )
 
 from qhana_plugin_runner.celery import CELERY
 from qhana_plugin_runner.db.models.tasks import ProcessingTask
-from qhana_plugin_runner.plugin_utils.entity_marshalling import ensure_dict, load_entities
+from qhana_plugin_runner.plugin_utils.entity_marshalling import (
+    ensure_dict,
+    load_entities,
+    save_entities,
+)
 from qhana_plugin_runner.requests import open_url
+from qhana_plugin_runner.storage import STORE
 
 from . import Optimizer
 
 TASK_LOGGER = get_task_logger(__name__)
+
+
+def poll_task(url):
+    while True:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+
+            response_data = response.json()
+
+            if response_data["status"] == "SUCCESS":
+                return response_data["log"]
+
+            sleep(1)
+        except requests.HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")
+        except requests.RequestException as req_err:
+            print(f"Request to API ended in an error: {req_err}")
+        except Exception as err:
+            print(f"Other error occurred: {err}")
 
 
 def get_features(ent: dict, target_variable_name: str) -> np.ndarray:
@@ -88,7 +118,7 @@ def get_features_and_target(
 
 
 @CELERY.task(name=f"{Optimizer.instance.identifier}.optimize", bind=True)
-def optimize_task(self, db_id: int) -> str:
+def optimize_task(self, db_id: int):
     TASK_LOGGER.info(f"Starting the optimization task with db id '{db_id}'")
     task_data: Optional[ProcessingTask] = ProcessingTask.get_by_id(id_=db_id)
 
@@ -116,4 +146,23 @@ def optimize_task(self, db_id: int) -> str:
 
     response = requests.post(minimize_endpoint_url, json=min_input_data)
 
-    return "opt_task"
+    result = poll_task(response.url)
+    result = ast.literal_eval(result)
+
+    schema = MinimizerResultSchema()
+    result: MinimizerResult = schema.load(result)
+
+    csv_attributes = [f"x_{i}" for i in range(len(result.weights))]
+
+    entities = [dict(zip(csv_attributes, result.weights.tolist()))]
+
+    with SpooledTemporaryFile(mode="w") as output:
+        save_entities(entities, output, "text/csv", attributes=csv_attributes)
+        STORE.persist_task_result(
+            db_id,
+            output,
+            "weights.csv",
+            "entity/vector",
+            "text/csv",
+        )
+    return "successfully minimized the loss function"
