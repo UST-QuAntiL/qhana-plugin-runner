86424b33c5eb41863ba098a62dc5397b371b0773
Matthias Weilinger <matthias.weilinger@gmx.de>
2023-06-12 12:12:05 +0200

    update copyright notice

--- plugins/optimizer/minimizer/__init__.py
+++ plugins/optimizer/minimizer/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2022 QHAna plugin runner contributors.
+# Copyright 2023 QHAna plugin runner contributors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
