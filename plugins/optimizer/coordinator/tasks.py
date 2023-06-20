1199912e1fdd76d206d4ffe65eea0d77b5ec9d2c
Matthias Weilinger <matthias.weilinger@gmx.de>
2023-06-20 17:46:04 +0200

    get minimization result via signal

--- plugins/optimizer/coordinator/tasks.py
+++ plugins/optimizer/coordinator/tasks.py
@@ -12,66 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from time import sleep
 from typing import Iterator, Optional
 
 import numpy as np
 import requests
-from celery.utils.log import get_task_logger
-
-from plugins.optimizer.coordinator.shared_schemas import (
-    MinimizerInputData,
-    MinimizerInputSchema,
-)
+from plugins.optimizer.coordinator import Optimizer
 from qhana_plugin_runner.api.tasks_api import TaskData, TaskStatusSchema
 from qhana_plugin_runner.celery import CELERY
 from qhana_plugin_runner.db.models.tasks import ProcessingTask
+
 from qhana_plugin_runner.plugin_utils.entity_marshalling import (
     ensure_dict,
     load_entities,
 )
 from qhana_plugin_runner.requests import open_url
+from celery.utils.log import get_task_logger
+
 from qhana_plugin_runner.storage import STORE
 
-from . import Optimizer
-
 TASK_LOGGER = get_task_logger(__name__)
-
-
-def poll_task(url: str) -> str:
-    """
-    Periodically send GET request to the specified URL and return the response if successful.
-
-    Args:
-        url: The URL to send requests to.
-
-    Returns:
-        The response log if the request is successful.
-
-    Raises:
-        An exception if an HTTP error or a Request error occurs, or a general error.
-    """
-    while True:
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-
-            response_schema = TaskStatusSchema()
-            response_data: TaskData = response_schema.load(response.json())
-
-            if response_data.status == "SUCCESS":
-                return response_data.outputs
-            if response_data.status == "FAILURE":
-                raise Exception(response_data.log)
-
-            sleep(1)
-        except requests.HTTPError as http_err:
-            print(f"HTTP error occurred: {http_err}")
-        except requests.RequestException as req_err:
-            print(f"Request to API ended in an error: {req_err}")
-        except Exception as err:
-            print(f"Other error occurred: {err}")
-            break
 
 
 def get_features(ent: dict, target_variable_name: str) -> np.ndarray:
@@ -143,16 +102,16 @@ def get_features_and_target(
     return np.array(x), np.array(y)
 
 
-@CELERY.task(name=f"{Optimizer.instance.identifier}.optimize", bind=True)
-def optimize_task(self, db_id: int) -> str:
+@CELERY.task(name=f"{Optimizer.instance.identifier}.echo_results", bind=True)
+def echo_results(self, db_id: int) -> str:
     """
-    Start an optimization task for the specified database id.
+    Start a minimization task for the specified database id.
 
     Args:
         db_id: The database id of the task.
 
     Returns:
-        A success message if the loss function is minimized successfully.
+        A string representation of the weights obtained from the minimization process.
 
     Raises:
         A KeyError if task data with the specified id cannot be loaded to read parameters.
@@ -165,29 +124,10 @@ def optimize_task(self, db_id: int) -> str:
         TASK_LOGGER.error(msg)
         raise KeyError(msg)
 
-    input_file_url: str = task_data.data.get("input_file_url")
-    target_variable_name: str = task_data.data.get("target_variable")
-    calc_loss_endpoint_url = task_data.data.get("calc_loss_endpoint_url")
-    minimize_endpoint_url = task_data.data.get("minimize_endpoint_url")
-
-    X, y = get_features_and_target(input_file_url, target_variable_name)
-
-    min_input_data = MinimizerInputSchema().dump(
-        MinimizerInputData(
-            x=X,
-            y=y,
-            calc_loss_endpoint_url=calc_loss_endpoint_url,
-        )
-    )
-
-    response = requests.post(minimize_endpoint_url, json=min_input_data)
-
-    data_outputs = poll_task(response.url)
-
-    TASK_LOGGER.info(f"data_outputs: {data_outputs}")
-
-    for output in data_outputs:
-        TASK_LOGGER.info(f"output: {output}")
+    url = task_data.data.get("minimizer_result_url")
+    minimizer_response = requests.get(url)
+    response_data: TaskData = TaskStatusSchema().load(minimizer_response.json())
+    for output in response_data.outputs:
         STORE.persist_task_result(
             task_db_id=db_id,
             file_=output["href"],
@@ -198,4 +138,4 @@ def optimize_task(self, db_id: int) -> str:
             commit=True,
         )
 
-    return "successfully minimized the loss function"
+    return "Success"
