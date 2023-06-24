8e6081ac695920857df876dc28724ccdf0baab6e
Matthias Weilinger <matthias.weilinger@gmx.de>
2023-06-24 15:50:40 +0200

    remove interaction demo plugin

--- qhana_plugin_runner/plugin_utils/url_utils.py
+++
@@ -1,7 +0,0 @@
-import urllib.parse
-
-
-def get_plugin_name_from_plugin_url(plugin_url: str) -> str:
-    decoded: str = urllib.parse.unquote(plugin_url).rstrip("/")
-    plugin_name: str = decoded.split("/")[-1]
-    return plugin_name
