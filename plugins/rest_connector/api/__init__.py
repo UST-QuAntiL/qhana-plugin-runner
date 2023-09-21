b43214fc4d48dd211f37ecb02b7618ef6c5b518a
Philipp Wundrack <philipp.wundrack@live.de>
2023-09-21 11:23:10 +0200

    fix error when installing dependencies

--- plugins/rest_connector/api/__init__.py
+++ plugins/rest_connector/api/__init__.py
@@ -1,1 +1,6 @@
-from . import connector_routes, edit_routes, main_routes
+try:
+    from . import connector_routes, edit_routes, main_routes
+except ImportError:
+    # When running `poetry run flask install`, importing the routes will fail, because the dependencies are not
+    # installed yet.
+    pass
