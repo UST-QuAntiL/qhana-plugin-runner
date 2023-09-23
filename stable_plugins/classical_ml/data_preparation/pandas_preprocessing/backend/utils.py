1c5385c9b45c6c2c6208fa806f386b4e25afde66
Benedikt Riegel <benedikt.riegel@gmail.com>
2023-09-23 19:35:27 +0200

    Simplified return conditions in utils.get_number_if_possible
    Co-authored-by: Fabian Bühler <fabian.buehler@iaas.uni-stuttgart.de>

--- stable_plugins/classical_ml/data_preparation/pandas_preprocessing/backend/utils.py
+++ stable_plugins/classical_ml/data_preparation/pandas_preprocessing/backend/utils.py
@@ -2,13 +2,10 @@ def get_number_if_possible(s: str):
     if s.isdigit():
         return int(s)
 
-    temp = None
     try:
         temp = float(s)
+        return temp
     except ValueError:
         pass
-
-    if temp is not None:
-        s = temp
-
+        
     return s
