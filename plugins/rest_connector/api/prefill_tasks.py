88fd0cc6b807b0a63d717181345d4a3a9f9da2af
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2023-10-04 15:47:05 +0200

    Prefill path variables from endpoint path

--- plugins/rest_connector/api/prefill_tasks.py
+++ plugins/rest_connector/api/prefill_tasks.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import re
 from typing import List, Optional
 
 from celery.utils.log import get_task_logger
@@ -22,6 +23,7 @@ from qhana_plugin_runner.requests import open_url
 
 from ..openapi import get_endpoint_paths
 from ..plugin import RESTConnector
+from .schemas import ConnectorKey
 
 TASK_LOGGER = get_task_logger(__name__)
 
@@ -56,12 +58,25 @@ def prefill_values(connector_id: str, last_step: str):
         return
     assert isinstance(connector, dict)
     new_data = dict(connector)
-    if last_step == "openapi-spec":
+    if last_step == ConnectorKey.OPENAPI_SPEC.value:
         spec = connector.get("openapi_spec_url")
         if spec and isinstance(spec, str):
             new_data["autocomplete_paths"] = list(get_endpoint_paths(spec))
-        PluginState.set_value(
-            parent_plugin.identifier, connector_id, new_data, commit=True
-        )
+            PluginState.set_value(
+                parent_plugin.identifier, connector_id, new_data, commit=True
+            )
+    if last_step == ConnectorKey.ENDPOINT_URL.value:
+        path = connector.get("endpoint_url")
+        if path and isinstance(path, str):
+            matches = re.findall(r"\{([\w\d]+)\}", path)
+            new_data["endpoint_variables"] = {var: "" for var in matches}
+            if not new_data["endpoint_variables"]:
+                finished = new_data.get("finished_steps", [])
+                assert isinstance(finished, list)
+                finished.append(ConnectorKey.ENDPOINT_VARIABLES.value)
+                new_data["finished_steps"] = finished
+            PluginState.set_value(
+                parent_plugin.identifier, connector_id, new_data, commit=True
+            )
 
     unlock_connector(connector_id=connector_id)
