0c4ab49247c5c054cfb415ad201a87e66ce9e607
Sevim Davul <67965984+svimdvl@users.noreply.github.com>
2024-01-31 13:38:18 +0100

    Update version to v1.0.0 in braket_local.py

--- amazonBraket_localSim/braket_local.py
+++ amazonBraket_localSim/braket_local.py
@@ -60,7 +60,7 @@ from qhana_plugin_runner.tasks import save_task_error, save_task_result
 from qhana_plugin_runner.util.plugins import QHAnaPluginBase, plugin_identifier
 
 _plugin_name = "AmazonBraket_LocalSimulator"
-__version__ = "v0.1.0"
+__version__ = "v1.0.0"
 _identifier = plugin_identifier(_plugin_name, __version__)
 
 
