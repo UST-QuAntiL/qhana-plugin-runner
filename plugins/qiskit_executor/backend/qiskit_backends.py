ebfab8663d384461ec5108063a58096eef0ce611
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2024-02-06 14:06:23 +0100

    Fix 401 http error not being handled in qiskit executor

--- plugins/qiskit_executor/backend/qiskit_backends.py
+++ plugins/qiskit_executor/backend/qiskit_backends.py
@@ -13,13 +13,13 @@
 # limitations under the License.
 
 from typing import Optional
+
 from celery.utils.log import get_task_logger
 from qiskit import IBMQ
 from qiskit.providers import BackendV1
-
 from qiskit.providers.exceptions import QiskitBackendNotFoundError
-from qiskit_ibm_provider import IBMProvider, IBMInputValueError, IBMBackend
-
+from qiskit_ibm_provider import IBMBackend, IBMInputValueError, IBMProvider
+from qiskit_ibm_provider.api.exceptions import RequestsApiError
 
 TASK_LOGGER = get_task_logger(__name__)
 
@@ -32,6 +32,12 @@ def get_provider(ibmq_token: str) -> Optional[IBMProvider]:
     """
     try:
         provider = IBMProvider(ibmq_token)
+    except RequestsApiError as err:
+        if err.status_code == 401:
+            TASK_LOGGER.info("User provided bad token!")
+        else:
+            TASK_LOGGER.info("Login failed!")
+        return None
     except IBMInputValueError:
         TASK_LOGGER.info("Login failed!")
         return None
