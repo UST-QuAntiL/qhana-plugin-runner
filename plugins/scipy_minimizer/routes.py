522f0c248dad41242322b2506b0e9604388ee55b
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2024-02-14 10:15:32 +0100

    Fix step not cleared early
    Add call to step update signal
    Fix redundant # signs in license header

--- plugins/scipy_minimizer/routes.py
+++ plugins/scipy_minimizer/routes.py
@@ -1,16 +1,16 @@
-# # Copyright 2023 QHAna plugin runner contributors.
-# #
-# # Licensed under the Apache License, Version 2.0 (the "License");
-# # you may not use this file except in compliance with the License.
-# # You may obtain a copy of the License at
-# #
-# #     http://www.apache.org/licenses/LICENSE-2.0
-# #
-# # Unless required by applicable law or agreed to in writing, software
-# # distributed under the License is distributed on an "AS IS" BASIS,
-# # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# # See the License for the specific language governing permissions and
-# # limitations under the License.
+# Copyright 2023 QHAna plugin runner contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 from http import HTTPStatus
@@ -300,7 +300,13 @@ class MinimizationEndpoint(MethodView):
         if initial_weights_url:
             db_task.data["initial_weights_url"] = initial_weights_url
 
+        db_task.clear_previous_step()
+
         db_task.save(commit=True)
+
+        app = current_app._get_current_object()
+        TASK_STEPS_CHANGED.send(app, task_id=db_task.id)
+
         task = minimize_task.s(db_id=db_task.id) | save_task_result.s(db_id=db_task.id)
 
         # save errors to db
