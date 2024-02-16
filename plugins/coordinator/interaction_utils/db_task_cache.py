dcd37bc629d7598029be0fc6d04d5d69fe224ad9
Philipp Wundrack <philipp.wundrack@live.de>
2024-02-16 11:28:07 +0100

    remove unused code

--- plugins/coordinator/interaction_utils/db_task_cache.py
+++ plugins/coordinator/interaction_utils/db_task_cache.py
@@ -1,32 +1,7 @@
-from typing import Optional
-from functools import lru_cache
-
 import sqlalchemy_json
 from celery.utils.log import get_task_logger
 
-from qhana_plugin_runner.db import DB
-from qhana_plugin_runner.db.models.tasks import ProcessingTask
-
 TASK_LOGGER = get_task_logger(__name__)
-
-
-@lru_cache
-def get_of_calc_data(db_id: int) -> tuple:
-    db_task: Optional[ProcessingTask] = ProcessingTask.get_by_id(id_=db_id)
-    if db_task is None:
-        msg = f"Could not load task data with id {db_id} to read parameters!"
-        TASK_LOGGER.error(msg)
-        raise KeyError(msg)
-
-    # Detach the entire db_task object from the session
-    DB.session.expunge(db_task)
-
-    # Convert TrackedDict and TrackedList to standard Python dict and list
-    x = convert_tracked_to_standard(db_task.data["x"])
-    y = convert_tracked_to_standard(db_task.data["y"])
-    hyperparameter = convert_tracked_to_standard(db_task.data["hyperparameter"])
-
-    return x, y, hyperparameter
 
 
 def convert_tracked_to_standard(data):
--- plugins/scipy_minimizer/shared/schemas.py
+++ plugins/scipy_minimizer/shared/schemas.py
@@ -22,25 +22,9 @@ class LossResponseData:
     loss: float
 
 
-class LossResponseSchema(MaBaseSchema):
-    loss = ma.fields.Float(required=True, allow_none=False)
-
-    @ma.post_load
-    def make_object(self, data, **kwargs):
-        return LossResponseData(**data)
-
-
 @dataclass
 class GradientResponseData:
     gradient: np.ndarray
-
-
-class GradientResponseSchema(MaBaseSchema):
-    gradient = NumpyArray(required=True, allow_none=False)
-
-    @ma.post_load
-    def make_object(self, data, **kwargs):
-        return GradientResponseData(**data)
 
 
 @dataclass
@@ -49,28 +33,10 @@ class LossAndGradientResponseData:
     gradient: np.ndarray
 
 
-class LossAndGradientResponseSchema(MaBaseSchema):
-    loss = ma.fields.Float(required=True, allow_none=False)
-    gradient = NumpyArray(required=True, allow_none=False)
-
-    @ma.post_load
-    def make_object(self, data, **kwargs):
-        return LossAndGradientResponseData(**data)
-
-
 @dataclass
 class TaskStatusChanged:
     url: str
     status: str
-
-
-class TaskStatusChangedSchema(MaBaseSchema):
-    url = ma.fields.Url(required=True, allow_none=True)
-    status = ma.fields.String(required=True, allow_none=False)
-
-    @ma.post_load
-    def make_object(self, data, **kwargs):
-        return TaskStatusChanged(**data)
 
 
 @dataclass
@@ -80,27 +46,9 @@ class CalcLossOrGradInput:
     y: Optional[np.ndarray] = None
 
 
-class CalcLossOrGradInputSchema(MaBaseSchema):
-    x: NumpyArray = NumpyArray(required=False, allow_none=True)
-    y: NumpyArray = NumpyArray(required=False, allow_none=True)
-    x0: NumpyArray = NumpyArray(required=True, allow_none=False)
-
-    @ma.post_load
-    def make_object(self, data, **kwargs):
-        return CalcLossOrGradInput(**data)
-
-
 @dataclass
 class ObjectiveFunctionCallbackData:
     calc_loss_endpoint_url: str
-
-
-class ObjectiveFunctionCallbackSchema(MaBaseSchema):
-    calc_loss_endpoint_url = ma.fields.Url(required=True, allow_none=False)
-
-    @ma.post_load
-    def make_object(self, data, **kwargs):
-        return ObjectiveFunctionCallbackData(**data)
 
 
 @dataclass
@@ -108,25 +56,9 @@ class ObjectiveFunctionInvokationCallbackData:
     task_id: int
 
 
-class ObjectiveFunctionInvokationCallbackSchema(MaBaseSchema):
-    task_id = ma.fields.Integer(required=True, allow_none=False)
-
-    @ma.post_load
-    def make_object(self, data, **kwargs):
-        return ObjectiveFunctionInvokationCallbackData(**data)
-
-
 @dataclass
 class MinimizerCallbackData:
     minimize_endpoint_url: str
-
-
-class MinimizerCallbackSchema(MaBaseSchema):
-    minimize_endpoint_url = ma.fields.Url(required=True, allow_none=False)
-
-    @ma.post_load
-    def make_object(self, data, **kwargs):
-        return MinimizerCallbackData(**data)
 
 
 @dataclass
@@ -138,29 +70,9 @@ class MinimizerInputData:
     callback_url: Optional[str] = None
 
 
-class MinimizerInputSchema(MaBaseSchema):
-    x0 = NumpyArray(required=True, allow_none=False)
-    calc_loss_endpoint_url = ma.fields.Url(required=True, allow_none=False)
-    calc_gradient_endpoint_url = ma.fields.Url(required=False, allow_none=True)
-    calc_loss_and_gradient_endpoint_url = ma.fields.Url(required=False, allow_none=True)
-    callback_url = ma.fields.Url(required=False, allow_none=True)
-
-    @ma.post_load
-    def make_object(self, data, **kwargs):
-        return MinimizerInputData(**data)
-
-
 @dataclass
 class MinimizerResult:
     weights: np.ndarray
-
-
-class MinimizerResultSchema(MaBaseSchema):
-    weights = NumpyArray(required=True, allow_none=False)
-
-    @ma.post_load
-    def make_object(self, data, **kwargs):
-        return MinimizerResult(**data)
 
 
 @dataclass
@@ -169,38 +81,11 @@ class ObjectiveFunctionPassData:
     y: np.ndarray
 
 
-class ObjectiveFunctionPassDataSchema(MaBaseSchema):
-    x = NumpyArray(required=True, allow_none=False)
-    y = NumpyArray(required=True, allow_none=False)
-
-    @ma.post_load
-    def make_object(self, data, **kwargs):
-        return ObjectiveFunctionPassData(**data)
-
-
 @dataclass
 class ObjectiveFunctionPassDataResponse:
     number_weights: int
 
 
-class ObjectiveFunctionPassDataResponseSchema(MaBaseSchema):
-    number_weights = ma.fields.Integer(
-        required=True, allow_none=False, data_key="numberWeights"
-    )
-
-    @ma.post_load
-    def make_object(self, data, **kwargs):
-        return ObjectiveFunctionPassDataResponse(**data)
-
-
 @dataclass
 class SingleNumpyArray:
     array: np.ndarray
-
-
-class SingleNumpyArraySchema(MaBaseSchema):
-    array = NumpyArray(required=True, allow_none=False)
-
-    @ma.post_load
-    def make_object(self, data, **kwargs):
-        return SingleNumpyArray(**data)
