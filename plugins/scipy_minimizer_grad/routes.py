70010acc67a1345aaca37dc2be6b4099b0557e99
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2024-02-20 15:00:48 +0100

    Remove dead code
    move callback url schema to other schemas of minimizer grad
    remove unused shared and interaction utils in minimizer grad

--- plugins/scipy_minimizer_grad/routes.py
+++ plugins/scipy_minimizer_grad/routes.py
@@ -38,8 +38,9 @@ from qhana_plugin_runner.tasks import (
 )
 
 from . import SCIPY_MINIMIZER_GRAD_BLP, ScipyMinimizerGrad
-from .interaction_utils.schemas import CallbackUrl, CallbackUrlSchema
 from .schemas import (
+    CallbackUrl,
+    CallbackUrlSchema,
     MinimizerEnum,
     MinimizerSetupTaskInputData,
     MinimizerSetupTaskInputSchema,
