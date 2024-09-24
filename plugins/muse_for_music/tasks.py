216d4c86e505b16aa0b6760ba901dc6a3ce24cf6
Fabian Bühler <fabian.buehler@iaas.uni-stuttgart.de>
2024-09-24 11:20:53 +0200

    Add plugin skeleton for MUSE4music data loader

---
+++ plugins/muse_for_music/tasks.py
@@ -0,0 +1,80 @@
+# Copyright 2024 QHAna plugin runner contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from json import loads as json_load
+from tempfile import SpooledTemporaryFile
+from typing import Optional
+
+from celery.utils.log import get_task_logger
+
+from qhana_plugin_runner.celery import CELERY
+from qhana_plugin_runner.db.models.tasks import ProcessingTask
+from qhana_plugin_runner.storage import STORE
+from qhana_plugin_runner.registry_client import PLUGIN_REGISTRY_CLIENT
+
+from requests import session
+
+from . import M4MLoaderPlugin
+from .schemas import InputParameters, InputParametersSchema
+
+TASK_LOGGER = get_task_logger(__name__)
+
+
+@CELERY.task(name=f"{M4MLoaderPlugin.instance.identifier}.import_data", bind=True)
+def import_data(self, db_id: int) -> str:
+    # get parameters
+
+    TASK_LOGGER.info(f"Starting new sql loader calculation task with db id '{db_id}'")
+    task_data: Optional[ProcessingTask] = ProcessingTask.get_by_id(id_=db_id)
+
+    if task_data is None:
+        msg = f"Could not load task data with id {db_id} to read parameters!"
+        TASK_LOGGER.error(msg)
+        raise KeyError(msg)
+
+    input_params: InputParameters = InputParametersSchema().loads(task_data.parameters)
+
+    TASK_LOGGER.info(f"Loaded input parameters from db: {str(input_params)}")
+
+    muse_for_music_url = None
+
+    if muse_for_music_url is None:
+        muse_for_music_url = get_muse_for_music_url_from_registry()
+
+    # FIXME implement
+
+    return "Import finished."
+
+
+def get_muse_for_music_url_from_registry() -> Optional[str]:
+    muse_for_music_url = None
+
+    with session():
+        service_result = PLUGIN_REGISTRY_CLIENT.search_by_rel(
+            ["service", "collection"], query_params={"service-id": "muse-for-music"}
+        )
+
+        if service_result and service_result.data.get("collectionSize") == 1:
+            service_api_link = service_result.data.get("items", [])[0]
+            if service_result.embedded:
+                service_object = service_result.embedded.get(service_api_link["href"])
+            else:
+                service_object = PLUGIN_REGISTRY_CLIENT.fetch_by_api_link(
+                    service_api_link
+                )
+            muse_for_music_url = (
+                service_object.data.get("url") if service_object else None
+            )
+
+    return muse_for_music_url
