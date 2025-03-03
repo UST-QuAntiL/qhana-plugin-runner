87fa7ec6a86cc4b908e34955609120210e078ac3
Fabian Bühler <fabian.buehler@iaas.uni-stuttgart.de>
2025-03-03 15:34:24 +0100

    Fix entity parsing in histogram visualization plugin

--- plugins/histogram_visualization/tasks.py
+++ plugins/histogram_visualization/tasks.py
@@ -14,6 +14,7 @@
 
 from tempfile import SpooledTemporaryFile
 from typing import Tuple
+from pathlib import Path
 
 import muid
 import pandas as pd
@@ -23,17 +24,13 @@ from requests.exceptions import HTTPError
 
 from qhana_plugin_runner.celery import CELERY
 from qhana_plugin_runner.db.models.virtual_plugins import DataBlob, PluginState
-from qhana_plugin_runner.requests import open_url_as_file_like
+from qhana_plugin_runner.requests import open_url, retrieve_filename, get_mimetype
 from qhana_plugin_runner.plugin_utils.entity_marshalling import load_entities, ensure_dict
 from qhana_plugin_runner.storage import STORE
 
 from . import HistogramVisualization
 
 TASK_LOGGER = get_task_logger(__name__)
-
-
-class PlotNotFinishedError(Exception):
-    pass
 
 
 def get_readable_hash(s: str) -> str:
@@ -51,10 +48,12 @@ def get_diagram(data_url: str, full_html: bool) -> Tuple[str, str]:
     Returns:
         Tuple[str, str]: html_content, filename
     """
-    with open_url_as_file_like(data_url) as (name, file_, mimetype):
+    with open_url(data_url) as response:
+        mimetype = get_mimetype(response)
         if mimetype is None:
             raise ValueError("Could not determine mimetype.")
-        data = next(ensure_dict(load_entities(file_, mimetype=mimetype)))
+        name = retrieve_filename(response)
+        data = next(ensure_dict(load_entities(response, mimetype=mimetype)))
 
     x_array = []
     y_array = []
@@ -81,7 +80,7 @@ def get_diagram(data_url: str, full_html: bool) -> Tuple[str, str]:
     )
     fig.update_traces(showlegend=False)
 
-    return fig.to_html(full_html=full_html), name
+    return fig.to_html(full_html=full_html), Path(name).stem
 
 
 @CELERY.task(
@@ -111,13 +110,7 @@ def generate_plot(self, data_url: str, hash_: str) -> str:
     return "Created plot!"
 
 
-@CELERY.task(
-    name=f"{HistogramVisualization.instance.identifier}.process",
-    bind=True,
-    autoretry_for=(PlotNotFinishedError,),
-    retry_backoff=True,
-    max_retries=None,
-)
+@CELERY.task(name=f"{HistogramVisualization.instance.identifier}.process", bind=True)
 def process(self, db_id: str, data_url: str) -> str:
     diagram, name = get_diagram(data_url, full_html=True)
 
