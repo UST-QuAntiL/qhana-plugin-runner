6eae3c898f6563925d0eee85fff9524e829dc1f0
Philipp Wundrack <philipp.wundrack@live.de>
2025-03-06 13:01:20 +0100

    fix dependency conflict

--- stable_plugins/visualization/complex/confusion_matrix/__init__.py
+++ stable_plugins/visualization/complex/confusion_matrix/__init__.py
@@ -55,7 +55,7 @@ class ConfusionMatrixVisualization(QHAnaPluginBase):
         return VIS_BLP
 
     def get_requirements(self) -> str:
-        return "pylatexenc~=2.10\nscipy~=1.10.1\nnumpy~=1.13"
+        return "pylatexenc~=2.10\nscipy~=1.10\nnumpy~=1.13"
 
 
 try:
