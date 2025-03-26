cee85bf44d4a2054968c730350ed409ce41fabd3
Philipp Wundrack <philipp.wundrack@live.de>
2025-03-26 13:51:45 +0100

    retrieve metadata from base data

--- plugins/data_join/tasks.py
+++ plugins/data_join/tasks.py
@@ -10,7 +10,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the Licens
+# limitations under the License.
 
 from json import dumps, loads
 from tempfile import SpooledTemporaryFile
@@ -20,12 +20,43 @@ from celery.utils.log import get_task_logger
 
 from qhana_plugin_runner.celery import CELERY
 from qhana_plugin_runner.db.models.tasks import ProcessingTask
+from qhana_plugin_runner.plugin_utils.entity_marshalling import (
+    ensure_dict,
+    load_entities,
+)
+from qhana_plugin_runner.requests import (
+    open_url,
+    retrieve_filename,
+    get_mimetype,
+    retrieve_data_type,
+    retrieve_attribute_metadata_url,
+)
 from qhana_plugin_runner.storage import STORE
 
 from . import DataJoin
 
 
 TASK_LOGGER = get_task_logger(__name__)
+
+
+def _get_file_metadata(entity_url: str) -> tuple[dict, list[str]]:
+    with open_url(entity_url) as response:
+        first_entity = next(
+            ensure_dict(
+                load_entities(response, get_mimetype(response)),
+            )
+        )
+        attributes = list(first_entity.keys())
+
+        metadata = {
+            "data": entity_url,
+            "attribute_metadata": retrieve_attribute_metadata_url(response),
+            "name": retrieve_filename(response),
+            "data_type": retrieve_data_type(response),
+            "content_type": get_mimetype(response),
+        }
+
+        return metadata, attributes
 
 
 @CELERY.task(name=f"{DataJoin.instance.identifier}.load_base", bind=True)
@@ -44,19 +75,12 @@ def load_base(self, db_id: int) -> str:
     assert data is not None
     assert isinstance(data, dict)
 
-    # TODO replace dummy data with actual implementation
-    data["base"] = {
-        "url": params["base"],
-        "attribute_metadata": "metadata-url",
-        "name": "BASE_Entities.json",
-        "data_type": "entity/list",
-        "content_type": "text/csv",
-    }
-    data["attributes"] = ["ID", "href", "name", "year", "reference_id", "something"]
+    base_data_url = params["base"]
+    data["base"], data["attributes"] = _get_file_metadata(base_data_url)
 
     task_data.save(commit=True)
 
-    return "TODO: Implement extracting information (i.e., attribute list, attribute metadata url, possible join targets) from base entities!"
+    return "Loaded metadata from base data finished."
 
 
 @CELERY.task(name=f"{DataJoin.instance.identifier}.add_data_to_join", bind=True)
@@ -81,21 +105,12 @@ def add_data_to_join(self, db_id: int, entity_url: str, join_attr: str):
         TASK_LOGGER.error(msg)
         raise KeyError(msg)
 
-    attribute_metadata_url = ""  # FIXME load URL from request
-    file_name = "JOIN_Entities.csv"  # FIXME load from request
+    file_metadata, _ = _get_file_metadata(entity_url)
+    file_metadata["join_attr"] = join_attr
 
     joins = data.setdefault("joins", [])
     assert isinstance(joins, list)
-    joins.append(
-        {
-            "data": entity_url,
-            "attribute_metadata": attribute_metadata_url,
-            "name": file_name,
-            "join_attr": join_attr,
-            "data_type": "entity/list",
-            "content_type": "text/csv",
-        }
-    )
+    joins.append(file_metadata)
 
     task_data.save(commit=True)
 
