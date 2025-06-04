e2ba533389bddebfbf967524044567aef700abd0
Lukas Joraschek <lukas.joraschek@gmail.com>
2025-06-04 15:24:08 +0200

    Final touch up and bump up version

--- plugins/qnn/state_preparation.py
+++ plugins/qnn/state_preparation.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 from http import HTTPStatus
-from json import dumps, loads
 from tempfile import SpooledTemporaryFile
 from typing import Mapping, Optional
 from enum import Enum
@@ -46,10 +45,10 @@ from qhana_plugin_runner.db.models.tasks import ProcessingTask
 from qhana_plugin_runner.storage import STORE
 from qhana_plugin_runner.tasks import save_task_error, save_task_result
 from qhana_plugin_runner.util.plugins import QHAnaPluginBase, plugin_identifier
-from qhana_plugin_runner.api.extra_fields import EnumField, CSVList
+from qhana_plugin_runner.api.extra_fields import EnumField
 
 _plugin_name = "state-preparation"
-__version__ = "v0.1"
+__version__ = "v0.2.0"
 _identifier = plugin_identifier(_plugin_name, __version__)
 
 
@@ -60,11 +59,11 @@ STATE_PREPARATION_BLP = SecurityBlueprint(
     template_folder="state_preparation_templates",
 )
 
+
 class METHODENUM(Enum):
     BASIS_ENCODING = "Basis Encoding"
     ANGLE_ENCODING = "Angle Encoding"
     ARBITRARY_STATE = "Arbitrary State"
-    
 
 
 class StatePreparationParametersSchema(FrontendFormBaseSchema):
@@ -78,7 +77,6 @@ class StatePreparationParametersSchema(FrontendFormBaseSchema):
             "input_type": "select",
         },
     )
-
 
     data_values = ma.fields.String(
         required=False,
@@ -119,8 +117,6 @@ class StatePreparationParametersSchema(FrontendFormBaseSchema):
     )
 
 
-
-
 @STATE_PREPARATION_BLP.route("/")
 class PluginsView(MethodView):
     """Plugins collection resource."""
@@ -159,13 +155,7 @@ class PluginsView(MethodView):
 class MicroFrontend(MethodView):
     """Micro frontend for the state preparation plugin."""
 
-    example_inputs = {
-        "inputStr": "Sample input string.",
-        "method": METHODENUM.BASIS_ENCODING,
-        "data_values": "0,1,0,1",
-        "digits_before_decimal": 2,
-        "digits_after_decimal": 2,
-    }
+    example_inputs = {}
 
     @STATE_PREPARATION_BLP.html_response(
         HTTPStatus.OK, description="Micro frontend of the state preparation plugin."
@@ -212,7 +202,7 @@ class MicroFrontend(MethodView):
                 values=data,
                 errors=errors,
                 process=url_for(f"{STATE_PREPARATION_BLP.name}.ProcessView"),
-                help_text="This is an example help text with basic **Markdown** support.",
+                help_text="",
                 example_values=url_for(
                     f"{STATE_PREPARATION_BLP.name}.MicroFrontend", **self.example_inputs
                 ),
@@ -224,16 +214,23 @@ class MicroFrontend(MethodView):
 class ProcessView(MethodView):
     """Start a long running processing task."""
 
-    @STATE_PREPARATION_BLP.arguments(StatePreparationParametersSchema(unknown=EXCLUDE), location="form")
+    @STATE_PREPARATION_BLP.arguments(
+        StatePreparationParametersSchema(unknown=EXCLUDE), location="form"
+    )
     @STATE_PREPARATION_BLP.response(HTTPStatus.SEE_OTHER)
     @STATE_PREPARATION_BLP.require_jwt("jwt", optional=True)
     def post(self, arguments):
         """Start the state_preparation task."""
-        db_task = ProcessingTask(task_name=prepare_task.name, parameters=StatePreparationParametersSchema().dumps(arguments))
+        db_task = ProcessingTask(
+            task_name=prepare_task.name,
+            parameters=StatePreparationParametersSchema().dumps(arguments),
+        )
         db_task.save(commit=True)
 
         # all tasks need to know about db id to load the db entry
-        task: chain = prepare_task.s(db_id=db_task.id) | save_task_result.s(db_id=db_task.id)
+        task: chain = prepare_task.s(db_id=db_task.id) | save_task_result.s(
+            db_id=db_task.id
+        )
         # save errors to db
         task.link_error(save_task_error.s(db_id=db_task.id))
         task.apply_async()
@@ -249,19 +246,20 @@ class StatePreparation(QHAnaPluginBase):
     name = _plugin_name
     version = __version__
     description = "Plugin for state preparation in quantum circuits."
-    tags = ["state-preparation", "qiskit", "qasm", "qasm-3"]
+    tags = ["state-preparation", "encoding", "qiskit-1.3.2", "qasm-3"]
 
     def __init__(self, app: Optional[Flask]) -> None:
         super().__init__(app)
 
     def get_api_blueprint(self):
         return STATE_PREPARATION_BLP
-    
+
     def get_requirements(self) -> str:
         return "qiskit~=1.3.2\nnumpy"
 
 
 TASK_LOGGER = get_task_logger(__name__)
+
 
 @CELERY.task(name=f"{StatePreparation.instance.identifier}.prepare_task", bind=True)
 def prepare_task(self, db_id: int) -> str:
@@ -276,21 +274,43 @@ def prepare_task(self, db_id: int) -> str:
         msg = f"Could not load task data with id {db_id} to read parameters!"
         TASK_LOGGER.error(msg)
         raise KeyError(msg)
-    
-    method: Optional[int] = StatePreparationParametersSchema().loads(task_data.parameters or "{}").get("preparation_method", None)
-    data_values: Optional[str] =  StatePreparationParametersSchema().loads(task_data.parameters or "{}").get("data_values", None)
-    digits_before_decimal: Optional[int] =  StatePreparationParametersSchema().loads(task_data.parameters or "{}").get("digits_before_decimal", None)
-    digits_after_decimal: Optional[int] =  StatePreparationParametersSchema().loads(task_data.parameters or "{}").get("digits_after_decimal", None)
-    encode_sign: Optional[bool] =  StatePreparationParametersSchema().loads(task_data.parameters or "{}").get("encode_sign", None)
 
+    method: Optional[int] = (
+        StatePreparationParametersSchema()
+        .loads(task_data.parameters or "{}")
+        .get("preparation_method", None)
+    )
+    data_values: Optional[str] = (
+        StatePreparationParametersSchema()
+        .loads(task_data.parameters or "{}")
+        .get("data_values", None)
+    )
+    digits_before_decimal: Optional[int] = (
+        StatePreparationParametersSchema()
+        .loads(task_data.parameters or "{}")
+        .get("digits_before_decimal", None)
+    )
+    digits_after_decimal: Optional[int] = (
+        StatePreparationParametersSchema()
+        .loads(task_data.parameters or "{}")
+        .get("digits_after_decimal", None)
+    )
+    encode_sign: Optional[bool] = (
+        StatePreparationParametersSchema()
+        .loads(task_data.parameters or "{}")
+        .get("encode_sign", None)
+    )
 
-    TASK_LOGGER.info(f"Loaded input parameters from db: data_values='{data_values}', digits_before_decimal='{digits_before_decimal}', digits_after_decimal='{digits_after_decimal}', encode_sign='{encode_sign}'")
+    TASK_LOGGER.info(
+        f"Loaded input parameters from db: data_values='{data_values}', digits_before_decimal='{digits_before_decimal}', digits_after_decimal='{digits_after_decimal}', encode_sign='{encode_sign}'"
+    )
 
-    #add brackets if not present
+    # add brackets if not present
     if not data_values[0] == "[":
         data_values = "[" + data_values + "]"
-    #convert string input to list
-    try: data_values = eval(data_values)
+    # convert string input to list
+    try:
+        data_values = eval(data_values)
     except Exception as e:
         TASK_LOGGER.error(f"Could not parse data values '{data_values}'! Error: {e}")
         raise ValueError(f"Could not parse data values '{data_values}'! Error: {e}")
@@ -304,34 +324,33 @@ def prepare_task(self, db_id: int) -> str:
         except ValueError:
             raise ValueError("The data value for basis encoding must be a number!")
 
-        
         if encode_sign:
             if value < 0:
                 value = -value
-                sign_bit = '1'
+                sign_bit = "1"
             else:
-                sign_bit = '0'
+                sign_bit = "0"
         else:
             if value < 0:
                 raise ValueError("Negative values are not allowed without sign encoding!")
-            sign_bit = ''
-
+            sign_bit = ""
 
         integer_part = int(value)
-        if integer_part > 2 ** digits_before_decimal - 1:
-            raise ValueError(f"Integer part {integer_part} is too large for {digits_before_decimal} bits!")
+        if integer_part > 2**digits_before_decimal - 1:
+            raise ValueError(
+                f"Integer part {integer_part} is too large for {digits_before_decimal} bits!"
+            )
         fractional_part = value - integer_part
 
-
         integer_bits = bin(integer_part)[2:].zfill(digits_before_decimal)
-        fractional_bits = bin(int(fractional_part * (2 ** digits_after_decimal)))[2:]
-        fractional_bits = fractional_bits.ljust(digits_after_decimal, '0')
+        fractional_bits = bin(int(fractional_part * (2**digits_after_decimal)))[2:]
+        fractional_bits = fractional_bits.ljust(digits_after_decimal, "0")
 
         bit_list = sign_bit + integer_bits + fractional_bits
 
         qc = QuantumCircuit(len(bit_list))
         for i, bit in enumerate(bit_list):
-            if bit == '1':
+            if bit == "1":
                 qc.x(i)
 
     elif method == METHODENUM.ANGLE_ENCODING:
@@ -339,34 +358,33 @@ def prepare_task(self, db_id: int) -> str:
             data_values = [float(x) for x in data_values]
         except ValueError:
             raise ValueError("Data values must be numbers!")
-        
+
         xmin = min(data_values)
         xmax = max(data_values)
         if (xmax - xmin) == 0:
             raise ValueError("Data values must not be all the same!")
-        normalized_values = [1/2 * np.pi * (x - xmin) / (xmax - xmin) for x in data_values]
+        normalized_values = [
+            1 / 2 * np.pi * (x - xmin) / (xmax - xmin) for x in data_values
+        ]
 
         qc = QuantumCircuit(len(data_values))
         for i, value in enumerate(normalized_values):
-            qc.ry(2*value, i)
+            qc.ry(2 * value, i)
 
-        
     elif method == METHODENUM.ARBITRARY_STATE:
         try:
-            data_values = [complex(real, imag) for real, imag  in data_values]
+            data_values = [complex(real, imag) for real, imag in data_values]
         except ValueError:
             raise ValueError("Data values must be complex numbers!")
-        gate=StatePreparationQiskit(data_values)
+        gate = StatePreparationQiskit(data_values)
         qc = QuantumCircuit(gate.num_qubits)
         qc.append(gate, range(gate.num_qubits))
-        #neccessary for arbitrary state as qasm import does not support complex numbers
+        # neccessary for arbitrary state as qasm import does not support complex numbers
         qc = qc.decompose()
     else:
         raise NotImplementedError(f"Method {method} not implemented!")
-    
-    qasm_str=qasm3.dumps(qc)
-        
 
+    qasm_str = qasm3.dumps(qc)
 
     with SpooledTemporaryFile(mode="w") as output:
         output.write(qasm_str)
