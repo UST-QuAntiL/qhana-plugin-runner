4373cef021b8f188c4c96137fd9906bdc9624de5
Sudotoyou <selintursu@Selins-MacBook-Pro.local>
2025-10-09 16:15:20 +0200

    add schemas.py and run black

--- plugins/low_code_modeler/schemas.py
+++ plugins/low_code_modeler/schemas.py
@@ -1,4 +1,3 @@
-
 import marshmallow as ma
 
 from qhana_plugin_runner.api.util import MaBaseSchema
@@ -6,15 +5,20 @@ from qhana_plugin_runner.api.util import MaBaseSchema
 
 class MetadataOfModellSchema(MaBaseSchema):
     """Metadaten von Modellen"""
+
     id = ma.fields.String()
     version = ma.fields.String()
     name = ma.fields.String()
     date = ma.fields.AwareDateTime()
     autosave = ma.fields.Bool(
-        required=False, missing=False, description="If this save was an autosave.")
+        required=False, missing=False, description="If this save was an autosave."
+    )
     model_id = ma.fields.String()
 
 
 class SaveModelParamsSchema(MaBaseSchema):
     """Parameter vom Speichervorgang"""
-    autosave = ma.fields.Bool(required=False, missing=False, description="Set this to true for autosaves.")
+
+    autosave = ma.fields.Bool(
+        required=False, missing=False, description="Set this to true for autosaves."
+    )
