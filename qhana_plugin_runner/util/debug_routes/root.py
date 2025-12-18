733ff9098757cca28c599ba49cbad2c48b82a3af
Fabian Bühler <fabian.buehler@iaas.uni-stuttgart.de>
2025-12-18 11:11:22 +0100

    Add microfrontend debug page to debugging routes

--- qhana_plugin_runner/util/debug_routes/root.py
+++ qhana_plugin_runner/util/debug_routes/root.py
@@ -17,10 +17,17 @@
 """Module for the root endpoint of the debug routes.
 Contains the blueprint to avoid circular dependencies."""
 
-from flask import Blueprint, render_template
+from json import dumps
+
+from flask import Blueprint, current_app, render_template, url_for
+from werkzeug.routing import converters
 
 DEBUG_BLP = Blueprint(
-    "debug-routes", __name__, template_folder="templates", url_prefix="/debug"
+    "debug-routes",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+    url_prefix="/debug",
 )
 
 
@@ -28,3 +35,98 @@ DEBUG_BLP = Blueprint(
 @DEBUG_BLP.route("/index")
 def index():
     return render_template("debug/index.html", title="QHAna Plugin Runner – Debug")
+
+
+MESSAGE_TEMPLATES = {
+    "prevent submit": "ui-prevent-submit",
+    "allow submit": "ui-allow-submit",
+    "get submit status": "ui-submit-status",
+    "load css": {
+        "type": "load-css",
+        "urls": [],
+    },
+    "data url response": {
+        "type": "data-url-response",
+        "inputKey": "",
+        "href": "",
+        "dataType": "entity/list",
+        "contentType": "text/csv",
+        "filename": "test.csv",
+        "version": "v1",
+    },
+    "plugin url response": {
+        "type": "plugin-url-response",
+        "inputKey": "",
+        "pluginUrl": "",
+        "pluginName": "Hello World",
+        "pluginVersion": "v1.0.0",
+    },
+    "implementations response": {
+        "type": "implementations-response",
+        "implementations": [
+            {"name": "test.qasm", "download": "http://", "version": 1, "type": "qasm??"}
+        ],
+    },
+    "autofill": {
+        "type": "autofill-response",
+        "value": "",
+        "encoding": "application/x-www-form-urlencoded",
+    },
+    "plugin context": {
+        "type": "plugin-context",
+        "experimentId": "",
+        "location": "navigation|workspace|experiment-navigation|timeline-step|data-preview",
+    },
+}
+
+
+@DEBUG_BLP.route("/frontend-debugger")
+def frontend_debugger():
+    """Debugger for Micro Frontends."""
+
+    micro_frontend_urls = []  # TODO
+
+    for rule in current_app.url_map.iter_rules():
+        if (
+            not rule.rule.startswith("/plugins/")
+            or rule.rule.count("/") < 3
+            or not rule.rule.split("/", maxsplit=3)[3]
+            or "GET" not in rule.methods
+        ):
+            continue
+
+        rule_converters = rule._converters
+
+        def replaced(argument: str):
+            argument_converter = rule_converters[argument]
+            if isinstance(argument_converter, converters.NumberConverter):
+                return 1
+            replacement = f"{{{{{argument}}}}}"
+
+            # check converter
+            argument_converter.to_url(replacement)
+            return replacement
+
+        try:
+            arguments = {a: replaced(a) for a in rule.arguments}
+        except Exception:
+            continue  # cannot build this URL, skip
+        url = url_for(rule.endpoint, **arguments, _external=True)
+        micro_frontend_urls.append(url)
+
+    if not MESSAGE_TEMPLATES["load css"]["urls"]:
+        MESSAGE_TEMPLATES["load css"]["urls"].append(
+            url_for(
+                "debug-routes.static",
+                filename="microfrontend-debug.css",
+                _external=True,
+            )
+        )
+
+    message_templates = {k: dumps(v, indent=4) for k, v in MESSAGE_TEMPLATES.items()}
+    return render_template(
+        "debug/microfrontend-debugger.html",
+        title="QHAna Plugin Runner – Micro Frontend Debugger",
+        micro_frontend_urls=micro_frontend_urls,
+        message_templates=message_templates,
+    )
